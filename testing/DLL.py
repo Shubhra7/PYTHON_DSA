7597ea4e48402c145e2d27a8afc5d99a2b027e76
Shubrha <shubhrajit.g2002@gmail.com>
2024-07-22 15:41:44 +0530

    done

--- testing/DLL.py
+++ testing/DLL.py
@@ -20,6 +20,25 @@ class DLL:
             self.start.prevlink=n
         self.start=n
     
+    def search(self,data):
+        p=self.start
+        while p is not None:
+            if p.data == data:
+                return p
+            p=p.nextlink
+        return None
+        
+    
+    def add_after(self,temp,data):
+        if temp is not None:
+            n=Node(data,temp,temp.nextlink)
+            if temp.nextlink is not None:
+                temp.nextlink.prevlink=n
+            temp.nextlink=n
+        else:
+            print("not present your data")
+
+    
     def display(self):
         p=self.start
         while p is not None:
@@ -30,4 +49,8 @@ obj=DLL()
 obj.add_beg(4)
 obj.add_beg(6)
 obj.add_beg(1)
+obj.display()
+
+print()
+obj.add_after(obj.search(4),99)
 obj.display()
