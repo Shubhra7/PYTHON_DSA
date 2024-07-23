a58cdc7c05e9bf132deae8cb70e19d60f78fda59
Shubrha <shubhrajit.g2002@gmail.com>
2024-07-23 23:54:07 +0530

    IsLand problem

---
+++ Tree/Island_Problem.py
@@ -0,0 +1,35 @@
+
+import collections
+
+visited=set()
+q=collections.deque()
+directions = [[1,0],[-1,0],[0,1],[0,-1]]
+
+def bfs(i,j):
+    q.append((i,j))
+    visited.add((i,j))
+    while q:
+        r,c = q.popleft()
+        for m,n in directions:
+            row = r+m
+            col = c+n
+            if(row in range(rows) and col in range(cols)
+               and grid[row][col]==1 and (row,col) not in visited):
+                visited.add((row,col))
+                q.append((row,col))
+
+grid = [[1,1,1,0,1,0,1],
+        [1,1,0,0,1,0,1],
+        [1,0,0,1,1,0,0]]
+rows=len(grid)
+cols=len(grid[0])
+
+count=0
+for i in range(rows):
+    for j in range(cols):
+        if( grid[i][j]==1 and (i,j) not in visited):
+            bfs(i,j)
+            count+=1
+
+print(count)
+        
