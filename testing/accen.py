c510b1a698e248c9f8393903fc0eaa83112ede39
Shubrha <shubhrajit.g2002@gmail.com>
2024-07-25 11:50:26 +0530

    2

--- testing/accen.py
+++ testing/accen.py
@@ -1,15 +1,10 @@
-r=int(input())
-unit=int(input())
-arr=list(map(int,input().split()))
-
-if (len(arr)==0):
-    print(-1)
-else:
-    total=r*unit
-    for i in range(len(arr)):
-        total-=arr[i]
-        if total<=0:
-            print(i+1)
-            break
-    else:
-        print(0)
+s="1C0C1C1A0B1"
+ans=int(s[0])
+for i in range(1,len(s),2):
+    if s[i]=='C':
+        ans=ans ^ int(s[i+1])
+    elif s[i]=='A':
+        ans=ans & int(s[i+1])
+    elif s[i]=='B':
+        ans=ans | int(s[i+1])
+print(ans)
