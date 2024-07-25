a25e11a15c254341310636d9ce0a71c885ba7d68
Shubrha <shubhrajit.g2002@gmail.com>
2024-07-25 11:37:49 +0530

    done sll

--- Tree/test.py
+++ Tree/test.py
@@ -1,38 +1,24 @@
-import collections
+class Node:
+    def __init__(self,data=None,link=None):
+        self.data=data
+        self.link=link
 
-q=collections.deque()
-
-def bfs(i,j):
-    q.append((i,j))
-    visited.add((i,j))
-    while q:
-        r,c = q.popleft()
-        for m,n in directions:
-            row= r+m
-            col=c+n
-            if (row in range(rows) and col in range(cols)
-                and grid[row][col]==1 and (row,col) not in visited):
-                q.append((row,col))
-                visited.add((row,col))
-        # print(q)
-        # print(visited)
-
-
-visited=set()
-directions=[[1,0],[-1,0],[0,1],[0,-1]]
-grid=[[1,1,1,0,1],
-      [1,1,0,0,1],
-      [1,0,0,0,0]]
-
-rows=len(grid)
-cols=len(grid[0])
-
-ans=0
-for i in range(rows):
-    for j in range(cols):
-        if(grid[i][j]==1 and (i,j) not in visited):
-            bfs(i,j)
-            ans+=1
-
-print("Total island: ",ans)
-print("BFS is: ",visited)
+class SLL:
+    def __init__(self):
+        self.start=None
+    def add_first(self,val):
+        n=Node(val)
+        n.link=self.start
+        self.start=n
+    
+    def display(self):
+        p=self.start
+        while p is not None:
+            print(p.data,end=" ")
+            p=p.link
+        
+obj=SLL()
+obj.add_first(4)
+obj.add_first(6)
+obj.add_first(7)
+obj.display()
