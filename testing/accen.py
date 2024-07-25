a6e37e88683043b75bde86c3f1c8a6ffcf1b9580
Shubrha <shubhrajit.g2002@gmail.com>
2024-07-25 12:05:58 +0530

    d

--- testing/accen.py
+++ testing/accen.py
@@ -1,10 +1,24 @@
-s="1C0C1C1A0B1"
-ans=int(s[0])
-for i in range(1,len(s),2):
-    if s[i]=='C':
-        ans=ans ^ int(s[i+1])
-    elif s[i]=='A':
-        ans=ans & int(s[i+1])
-    elif s[i]=='B':
-        ans=ans | int(s[i+1])
-print(ans)
+
+
+def check(s):
+    ndig=0
+    cap=0
+    if(len(s)>=4):
+        if not s[0].isdigit():
+            for i in range(len(s)):
+                if( s[i]==' ' or s[i]=='/'):
+                    return 0
+                if(s[i]>='A' and s[i]<='Z'):
+                    cap+=1
+                if s[i].isdigit():
+                    ndig+=1
+            if (cap >0 and ndig>0):
+                return 1
+        else:
+            return 0
+    else:
+        return 0
+
+
+print(check("aA1_67"))
+print(check("a987 abC012"))
