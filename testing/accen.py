dc8849bbc2b21441cc69d5a3205390f10ff095c2
Shubrha <shubhrajit.g2002@gmail.com>
2024-07-30 03:20:05 +0530

    ok

--- testing/accen.py
+++ testing/accen.py
@@ -1,18 +1,25 @@
 
-def OptBin(st):
-    ans=int(st[0])
-    for i in range(1,len(st),2):
-        if st[i] == 'A':
-            ans=ans & int(st[i+1])
-        elif st[i] == 'B':
-            ans=ans | int(st[i+1])
-        elif st[i] == 'C':
-            ans = ans ^ int(st[i+1])
-    return ans
+def CheckPassword(st):
+    if (len(st) >= 4):
+        if not st[0].isdigit():
+            cap=0
+            num=0
+            for i in range(len(st)):
+                if (st[i]==' ' or st[i]=='/'):
+                    print("shubhra")
+                    return 0
+                if (st[i]>='A' and st[i]<='Z'):
+                    cap+=1
+                if (st[i].isdigit()):
+                    num+=1
+            if( cap>0 and num>0):
+                return 1
+        else:
+            print("kalu1")
+            return 0
+    else:
+        print("kalu1")
+        return 0 
 
-
-st="1C0C1C1A0B1"
-print(OptBin(st))
-
-kalu="0C1A1B1C1C1B0A0"
-print(OptBin(kalu))
+st="a987 abC012"
+print(CheckPassword(st))
