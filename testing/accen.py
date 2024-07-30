a508bcb581f193a8dac223b7b847b8443b04e19d
Shubrha <shubhrajit.g2002@gmail.com>
2024-07-31 00:51:50 +0530

    7 done

--- testing/accen.py
+++ testing/accen.py
@@ -1,20 +1,16 @@
 
-def LargeSmall(arr):
-    if (len(arr)<=3):
-        return 0
+def ProductSmallest(sum,arr):
+    if (len(arr)<2):
+        return -1
+    arr.sort()
+    if ((arr[0] + arr[1]) <= sum):
+        return (arr[0]*arr[1])
     else:
-        even=[]
-        odd=[]
-        for i in range(len(arr)):
-            if (i%2==0):
-                even.append(arr[i])
-            else:
-                odd.append(arr[i])
-        even.sort()
-        odd.sort()
-        return (even[-2] + odd[1])
+        return 0
 
 
 
-arr =list(map(int,input().split()))
-print(LargeSmall(arr))
+sum = 4
+arr=list(map(int,input().split()))
+
+print(ProductSmallest(sum,arr))
