9e8ea574b4028a034aab9bc89f54099115a5f79c
Shubhrajit Ghosh <112610308+Shubhra7@users.noreply.github.com>
2024-08-03 07:33:10 +0000

    done 13

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,4 +1,20 @@
-st = input()
+def ex(i):
+    ans=0
+    while (i % 2==0):
+        ans+=1
+        i=i // 2
+    return ans
 
-ans = st.replace('p','*').replace('a','p').replace('*','a')
-print(ans)
+def MaxExpo(a,b):
+    max=0
+    ans=a
+    for i in range(a,b+1):
+        expo = ex(i)
+        if (expo > max):
+            max=expo
+            ans = i
+    return ans
+
+
+print(MaxExpo(7,12))
+print(MaxExpo(9,17))
