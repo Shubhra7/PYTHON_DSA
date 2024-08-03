a11238efcf6acfdae3cfafe63daa840cf3d79bad
Shubhrajit Ghosh <112610308+Shubhra7@users.noreply.github.com>
2024-08-03 06:58:11 +0000

    done 7

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,18 +1,11 @@
-
-def largeSmall(arr):
-    even=[]
-    odd=[]
-    for i in range(len(arr)):
-        if (i % 2 == 0):
-            even.append(arr[i])
-        else:
-            odd.append(arr[i])
-    even.sort()
-    odd.sort()
-    return even[-2] + odd[1]
-
-
+def ProSma(sum,arr):
+    if (len(arr) == 0):
+        return -1
+    arr.sort()
+    if ((arr[0] + arr[1]) <= sum):
+        return arr[0]*arr[1]
+    return 0
 
 
 arr=list(map(int,input().split()))
-print(largeSmall(arr))
+print(ProSma(6,arr))
