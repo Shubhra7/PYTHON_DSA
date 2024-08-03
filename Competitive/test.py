54d29024ba398793bdb805833d47aaf0f7096cdf
Shubhrajit Ghosh <112610308+Shubhra7@users.noreply.github.com>
2024-08-03 06:17:16 +0000

    done 1

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,33 +1,17 @@
-import collections
 
-q=collections.deque()
-visited=set()
-direction = [[1,0],[-1,0],[0,1],[0,-1]]
+def find(total,arr):
+    sum=0
+    for i in range(len(arr)):
+        sum += arr[i]
+        if (sum >= total):
+            return i+1
+    return -1
+        
 
-def bfs(i,j):
-    q.append((i,j))
-    visited.add((i,j))
-    while q:
-        r,c=q.popleft()
-        for m,n in direction:
-            rows = r + m
-            cols = c + n
-            if (rows in range(row) and cols in range(col) and 
-                grid[rows][cols] == 1 and (rows,cols) not in visited):
-                q.append((rows,cols))
-                visited.add((rows,cols))
+r=int(input("Enter:"))
+unit=int(input())
+arr=list(map(int,input().split()))
 
-grid=[[1,1,0,1],
-      [1,1,0,1],
-      [1,0,0,1]]
+total = r*unit
 
-row=len(grid)
-col=len(grid[0])
-
-count=0
-for i in range(row):
-    for j in range(col):
-        if (grid[i][j] == 1 and (i,j) not in visited):
-            bfs(i,j)
-            count+=1
-print(count)
+print(find(total,arr))
