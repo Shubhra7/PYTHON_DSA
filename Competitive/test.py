a5d3c51507d51f39dd57db8c31a47a0a91c235e9
Shubhrajit Ghosh <112610308+Shubhra7@users.noreply.github.com>
2024-08-03 07:20:49 +0000

    done 10

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,10 +1,16 @@
-s=input()
-ans=""
-count=0
-for i in range(len(s)):
-    if (s[i] == '-'):
-        count +=1
-    else:
-        ans += s[i]
 
-print('-'*count + ans)
+def NumOfCar(num1,num2):
+    carry=0
+    count=0
+    while (num1 != 0 or num2!=0):
+        n1 = num1 % 10
+        n2 = num2 % 10
+        val = n1 + n2 + carry
+        if (val > 9):
+            carry = val // 10
+            count += 1
+        num1 , num2 = num1 // 10, num2 // 10
+    return count
+
+print(NumOfCar(451,349))
+print(NumOfCar(23,563))
