b0187d862ccf3c708d3554db0ca1b11f419b3b10
Shubhrajit Ghosh <112610308+Shubhra7@users.noreply.github.com>
2024-08-03 07:25:19 +0000

    12 done

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,16 +1,4 @@
+st = input()
 
-def NumOfCar(num1,num2):
-    carry=0
-    count=0
-    while (num1 != 0 or num2!=0):
-        n1 = num1 % 10
-        n2 = num2 % 10
-        val = n1 + n2 + carry
-        if (val > 9):
-            carry = val // 10
-            count += 1
-        num1 , num2 = num1 // 10, num2 // 10
-    return count
-
-print(NumOfCar(451,349))
-print(NumOfCar(23,563))
+ans = st.replace('p','*').replace('a','p').replace('*','a')
+print(ans)
