ed8daa282cf85c14babb2033215ac0bce5db8c2d
Shubhrajit Ghosh <112610308+Shubhra7@users.noreply.github.com>
2024-08-03 06:45:38 +0000

    done 5

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,17 +1,14 @@
-def findCount(arr,num,diff):
-    count=0
-    for i in range(len(arr)):
-        if (abs(arr[i] - num) <= diff):
-            print(arr[i])
-            count+=1
-    if (count > 0):
-        return count
-    else:
-        return -1
+def diffof(n,m):
+    div=[]
+    nondiv=[]
+    for i in range(1,m+1):
+        if (i%n == 0):
+            div.append(i)
+        else:
+            nondiv.append(i)
+    return abs(sum(nondiv) - sum(div))
 
 
-arr=list(map(int,input().split()))
-num=13
-diff=2
 
-print(findCount(arr,num,diff))
+print(diffof(3,10))
+print(diffof(4,20))
