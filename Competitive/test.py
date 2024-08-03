73fbde2caaac984a150a153786b74d72c55c1f65
Shubhrajit Ghosh <112610308+Shubhra7@users.noreply.github.com>
2024-08-03 08:30:54 +0000

    done

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,30 +1,12 @@
-def search(arr,target,left,right):
-    if (left > right):
-        return -1
-    mid = left + (right - left)//2
-    if (arr[mid] == target):
-        return mid
-    if (arr[left] <= arr[mid]):
-        kalu.append(1)
-        if (arr[left] <= target and arr[mid]>= target):
-            return search(arr,target,left,mid-1)
-        else:
-            return search(arr,target,mid+1,right)
-    else:
-        golu.append(1)
-        if (arr[mid]<=target and arr[right]>=target):
-            return search(arr,target,mid+1,right)
-        else:
-            return search(arr,target,left,mid-1)
+def maxSubarraySum(arr):
+    cur_max=arr[0]
+    ans=arr[0]
+    for i in range(1,len(arr)):
+        cur_max = max(arr[i], arr[i]+cur_max)
+        ans=max(ans,cur_max)
+    return ans
 
 
 
-kalu=[]
-golu=[]
-# arr = [10,11,0,1,2,6,9]
-# print(search(arr,11,0,len(arr)-1))
-# print(search(arr,2,0,len(arr)-1))
-arr=[7,8,9,0,1,2,3,4,5,6]
-print(search(arr,9,0,len(arr)-1))
-print(kalu)
-print(golu)
+arr=[5,4,-1,7,8]
+print(maxSubarraySum(arr))
