9672c1844626288f86bbb5c0ebf2e3325f9fb480
Shubhrajit Ghosh <112610308+Shubhra7@users.noreply.github.com>
2024-08-03 07:46:30 +0000

    done diagonal sum

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,10 +1,16 @@
-def cal(m,n):
-    ans=[]
-    for i in range(m,n+1):
-        if (i%3 ==0 and i%5==0):
-            ans.append(i)
-    return sum(ans)
+grid=[[1,2,3,4],
+      [2,1,3,6],
+      [3,0,0,1],
+      [0,0,0,0]]
 
+sum = 0
+n=len(grid[0])
+re=[]
+for i in range(len(grid)):
+        for j in range(len(grid[0])):
+                if (i==j or (i+j+1)==n):
+                        re.append(grid[i][j])
+                        sum = sum + grid[i][j]
 
-print(cal(12,50))
-print(cal(100,160))
+print(sum)
+print(re)
