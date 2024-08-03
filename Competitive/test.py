18cf942dc847b72135bb0fb088864e21c6cbe2c1
Shubhrajit Ghosh <112610308+Shubhra7@users.noreply.github.com>
2024-08-03 06:33:33 +0000

    done 3

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,12 +1,18 @@
-s=input()
-ans=int(s[0])
-for i in range(1,len(s),2):
-    if (s[i] == 'A'):
-        ans &= int(s[i+1])
-    elif (s[i] == 'B'):
-        ans |= int(s[i+1])
-    elif (s[i] == 'C'):
-        ans ^= int(s[i+1])
-    
-print(ans)
+def CheckPass(s):
+    if (len(s) >=4 ):
+        if not s[0].isdigit():
+            num=0
+            cap=0
+            for i in s:
+                if (i.isdigit()):
+                    num+=1
+                if (i>='A' and i<='Z'):
+                    cap+=1
+                if (i==' ' or i=='/'):
+                    return 0
+            if (num>0 and cap>0):
+                return 1
+    return 0
 
+s=input()
+print(CheckPass(s))
