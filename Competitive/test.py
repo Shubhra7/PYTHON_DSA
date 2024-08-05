203c6f063e3781c6b6208481a397feb45ba2ff6d
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-05 11:40:10 +0530

    hm

--- Competitive/test.py
+++ Competitive/test.py
@@ -22,16 +22,4 @@ obj.add_beg(5)
 obj.add_beg(7)
 obj.display()
 
-def maxSubarraySum(arr):
-    cur_max=arr[0]
-    ans=arr[0]
-    for i in range(1,len(arr)):
-        cur_max = max(arr[i], arr[i]+cur_max)
-        ans=max(ans,cur_max)
-    return ans
-
-
-
-arr=[5,4,-1,7,8]
-print(maxSubarraySum(arr))
 
