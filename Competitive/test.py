d0f8853e14e3e59f2abe738254b6b642039b1d5c
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-05 11:43:00 +0530

    done

--- Competitive/test.py
+++ Competitive/test.py
@@ -15,11 +15,27 @@ class SLL:
             print(p.data,end=" ")
             p=p.link
         print()
+    def isEmpty(self):
+        if self.start is None:
+            return True
+        else:
+            return False
+    def add_end(self,val):
+        p=self.start
+        while p.link is not None:
+            p=p.link
+        n=Node(val)
+        p.link=n
+        
 
 obj=SLL()
 obj.add_beg(4)
 obj.add_beg(5)
 obj.add_beg(7)
+
+obj.add_end(1)
+obj.add_end(10)
+obj.add_end(77)
 obj.display()
 
 
