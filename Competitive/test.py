c485a6f64c6fb92464decd91621839df715011ce
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-07 20:07:11 +0530

    okay

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,20 +1,23 @@
-def check(st):
-    if (len(st) >= 6):
-        sm=0
-        cap=0
-        no=0
-        for i in st:
-            if (i>='a' and i<='z'):
-                sm+=1
-            elif (i>='A' and i<='Z'):
-                cap+=1
-            elif (i==' ' or i=='/'):
-                return "Invaild password"
-            elif (i.isdigit()):
-                no+=1
-        if (no >1 and sm >0 and cap > 0):
-            return "password valid"
-    return "Invalid password"
+def pasRec(s):
+    m=s[::-1]
+    i=0
+    j=2
+    n=len(s)
+    ans=""
+    while (j <= n and i<=j ):
+        val = int(m[i:j])
+        if (val >= 65 and val <=90):
+            ans = ans + chr(val)
+            i=j
+            j=j+2
+        elif (val >= 97 and val <= 122):
+            ans = ans + chr(val)
+            i=j
+            j=j+2
+        else:
+            j=j+1
+    return ans
 
-st = input()
-print(check(st))
+
+s=input()
+print(pasRec(s))
