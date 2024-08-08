e99507d3792c3f1174e42376e6f882d486ad798c
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-08 13:01:23 +0530

    island done

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,5 +1,19 @@
 import collections 
 
+def bfs(i,j):
+    q.append((i,j))
+    visited.add((i,j))
+    while q:
+        r,c = q.popleft()
+        for m,n in directions:
+            ro = r + m
+            co = c + n
+            if (ro in range(row) and co in range(col) and
+                grid[ro][co]==1 and (ro,co) not in visited):
+                visited.add((ro,co))
+                q.append((ro,co))
+
+
 grid = [[1,0,1,1,1],
         [1,0,0,1,1],
         [0,0,0,0,1]]
@@ -13,6 +27,8 @@ col = len(grid[0])
 count=0
 for i in range(row):
     for j in range(col):
-        if (grid[i][j] == 1):
+        if (grid[i][j] == 1 and (i,j) not in visited):
             bfs(i,j)
             count+=1
+
+print(count)
