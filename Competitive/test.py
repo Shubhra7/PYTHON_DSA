4dd771e9cbc3b3e870190815a201207e3ee756e5
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-13 10:48:57 +0530

    modified binary search done

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,13 +1,27 @@
-def maxSubarraySum(arr,n):
-    cur_max=arr[0]
-    ans=0
-    for i in range(1,len(arr)):
-        cur_max = max(arr[i], cur_max + arr[i])
-        ans = max(ans, cur_max)
-    return ans
+def modified_bin(arr,target,left,right):
+    if (left > right):
+        return -1
+    mid = left + (right - left)//2
+    if (arr[mid] == target):
+        return mid
+    elif (arr[left] <= arr[mid-1]):
+        print("hi1")
+        if (arr[left]>=target and arr[mid -1]>=target):
+            return modified_bin(arr,target,left,mid-1)
+        else:
+            return modified_bin(arr,target,mid+1,right)
+    else:
+        print("hi1")        
+        if (arr[mid+1]>=target and arr[right]>=target):
+            return modified_bin(arr,target,mid+1,right)
+        else:
+            return modified_bin(arr,target,left,mid-1)
+    
+
+def search(arr,target):
+    return modified_bin(arr,target,0,len(arr)-1)
 
 
-arr=[5,4,-1,7,8]
-print(maxSubarraySum(arr,10))
-arr1=[-5,-4,-1,-7,-8]
-print(maxSubarraySum(arr1,10))
+arr=[7,8,9,0,1,2,3,4,5,6]
+print(search(arr,8))
+print(search(arr,2))
