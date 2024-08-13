d60aaa70ac236b0d85b6851c4a3b5006c1b2d07f
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-13 10:33:52 +0530

    kadane done

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,37 +1,13 @@
-import collections
-
-def bfs(i,j):
-    q.append((i,j))
-    visited.add((i,j))
-    while q:
-        r,c = q.popleft()
-        for m,n in directions:
-            ro = r+m
-            co = c+n
-            if (ro in range(row) and co in range(col) and
-                grid[ro][co]==1 and (ro,co) not in visited):
-                q.append((ro,co))
-                visited.add((ro,co))
-
-    
+def maxSubarraySum(arr,n):
+    cur_max=arr[0]
+    ans=0
+    for i in range(1,len(arr)):
+        cur_max = max(arr[i], cur_max + arr[i])
+        ans = max(ans, cur_max)
+    return ans
 
 
-
-grid = [[1,0,0,1,1],
-        [1,0,1,1,1],
-        [0,0,0,0,1]]
-
-directions = [[1,0],[-1,0],[0,1],[0,-1]]
-q=collections.deque()
-visited=set()
-
-row=len(grid)
-col=len(grid[0])
-
-count=0
-for i in range(row):
-    for j in range(col):
-        if (grid[i][j]==1 and (i,j) not in visited):
-            bfs(i,j)
-            count+=1
-print(count)
+arr=[5,4,-1,7,8]
+print(maxSubarraySum(arr,10))
+arr1=[-5,-4,-1,-7,-8]
+print(maxSubarraySum(arr1,10))
