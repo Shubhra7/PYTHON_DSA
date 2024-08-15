1c33d1ad8c41aa36b0461ae026d266bb7d040f6c
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-16 00:06:45 +0530

    median of rowwise sorted matrix done

--- Competitive/Striver/median_in_row-wise-sorted_matrix.py
+++ Competitive/Striver/median_in_row-wise-sorted_matrix.py
@@ -4,10 +4,48 @@
 
 ## Problem Statement:: https://www.naukri.com/code360/problems/median-of-a-row-wise-sorted-matrix_1115473
 
+## 1) First take the lowest and highest element from the first element and last elements from subarrays
+    # 2)calculate the req '<=' element by = (m*n)//2
+    # 3)Then implement the binary search 
+    # 4) use upperbound()
+import sys
+import bisect
+
+def upperBound(mat,x,m):
+    ans = bisect.bisect_right(mat,x)
+    return ans
+
+def countSmallEqual(mat,m,n,x):
+    count = 0
+    for i in range(m):
+        count += upperBound(mat[i],x,m)
+    return count
+
+def median(mat,m,n):
+    low = min([mat[i][0] for i in range(len(mat))])
+    high = max([mat[i][n-1] for i in range(len(mat))])
+    # print(low)
+    # print(high)
+    req = (m * n)//2
+    while (low <= high):
+        mid = low + (high - low)//2
+        smallEqual = countSmallEqual(mat,m,n,mid)
+        if(smallEqual <= req):
+            low = mid + 1
+        else:
+            high = mid - 1
+    return low
+
+
+
+
+
 matrix = [     [ 1, 5, 7, 9, 11 ],
       [ 2, 3, 4, 8, 9 ],
       [ 4, 11, 14, 19, 20 ],
       [ 6, 10, 22, 99, 100 ],
       [ 7, 15, 17, 24, 28 ]   ]
 
+
+print(median(matrix,len(matrix),len(matrix[0])))
 
