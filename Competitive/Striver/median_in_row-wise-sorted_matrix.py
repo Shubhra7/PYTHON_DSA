22a58b9130113dae2263f4c7f2c39709c9e08e6b
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-15 23:49:00 +0530

    start

---
+++ Competitive/Striver/median_in_row-wise-sorted_matrix.py
@@ -0,0 +1,13 @@
+#********* Median in a row-wise sorted Matrix *********
+
+### Striver video link: https://youtu.be/Q9wXgdxJq48
+
+## Problem Statement:: https://www.naukri.com/code360/problems/median-of-a-row-wise-sorted-matrix_1115473
+
+matrix = [     [ 1, 5, 7, 9, 11 ],
+      [ 2, 3, 4, 8, 9 ],
+      [ 4, 11, 14, 19, 20 ],
+      [ 6, 10, 22, 99, 100 ],
+      [ 7, 15, 17, 24, 28 ]   ]
+
+
