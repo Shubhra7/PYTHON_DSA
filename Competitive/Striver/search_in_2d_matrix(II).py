39887af4319cfb2dda77da7865f3518180d92cae
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-16 10:51:03 +0530

    done

---
+++ Competitive/Striver/search_in_2d_matrix(II).py
@@ -0,0 +1,8 @@
+# Link: https://youtu.be/9ZbB397jU4k
+# Problem : https://leetcode.com/problems/search-a-2d-matrix-ii/description/
+
+grid = [[1,4,7,11,15],
+        [2,5,8,12,19],
+        [3,6,9,16,22],
+        [10,13,14,17,24],
+        [18,21,23,26,30]]
