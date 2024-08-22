c5a9f6b1cb9c93caf500315c673315c9ab4e7c23
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-22 12:37:57 +0530

    debug lets

--- Competitive/Search_in_rotated_array.py
+++ Competitive/Search_in_rotated_array.py
@@ -35,5 +35,5 @@ class Solution:
 
 obj=Solution()
 arr=[7,8,9,0,1,2,3,4,5,6]
-print(obj.search(arr,8))
+print(obj.search(arr,6))
         
--- Competitive/test.py
+++ Competitive/test.py
@@ -1,5 +1,21 @@
-def search(arr,item):
+def search(arr,left,right,item):
+    if (left <= right):
+        mid = left  + (right-left)//2
+        if (arr[mid] == item):
+            return mid
+        if(arr[left] <= arr[mid]):
+                if(arr[left]<= item and arr[mid]>=item):
+                        return search(arr,left,mid-1,item)
+                else:
+                        search(arr,mid+1,right,item)
+        else:
+                if(arr[mid]<=item and arr[right]>=item):
+                        return search(arr,mid+1,right,item)
+                else:
+                        return search(arr,left,mid-1,item)
+    else:
+        return -1
     
 
 arr=[7,8,9,0,1,2,3,4,5,6]
-print(search(arr,2))
+print(search(arr,0,len(arr)-1,9))
