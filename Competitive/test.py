f443d192ab5d4b383fd7978cbabaf3af0dc91d28
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-22 21:03:31 +0530

    hm

--- Competitive/test.py
+++ Competitive/test.py
@@ -20,7 +20,7 @@ arr=[7,8,9,0,1,2,3,4,5,6]
 print(search(arr,0,len(arr)-1,9))
 print(search(arr,0,len(arr)-1,0))
 print(search(arr,0,len(arr)-1,1))
-print(search(arr,0,len(arr)-1,2))
+print(search(arr,0,len(arr)-1,2)) 
 print(search(arr,0,len(arr)-1,3))
 print(search(arr,0,len(arr)-1,4))
 print(search(arr,0,len(arr)-1,5))
