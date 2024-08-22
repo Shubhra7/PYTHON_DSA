2a1ea4b26537031238aa3ecb8716cc8c7b93902e
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-22 12:49:29 +0530

    done

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,21 +1,28 @@
 def search(arr,left,right,item):
-    if (left <= right):
-        mid = left  + (right-left)//2
-        if (arr[mid] == item):
-            return mid
-        if(arr[left] <= arr[mid]):
-                if(arr[left]<= item and arr[mid]>=item):
-                        return search(arr,left,mid-1,item)
-                else:
-                        search(arr,mid+1,right,item)
-        else:
-                if(arr[mid]<=item and arr[right]>=item):
-                        return search(arr,mid+1,right,item)
-                else:
-                        return search(arr,left,mid-1,item)
-    else:
+    if(left > right):
         return -1
-    
+    mid = left + (right - left)//2
+    if(arr[mid]==item):
+        return mid
+    if(arr[left] <= arr[mid]):
+        if(arr[left]<=item and item<=arr[mid]):
+            return search(arr,left,mid-1,item)
+        else:
+            return search(arr,mid+1,right,item)
+    else:
+        if(arr[mid]<=item and item<=arr[right]):
+            return search(arr,mid+1,right,item)
+        else:
+            return search(arr,left,mid-1,item)
+
 
 arr=[7,8,9,0,1,2,3,4,5,6]
 print(search(arr,0,len(arr)-1,9))
+print(search(arr,0,len(arr)-1,0))
+print(search(arr,0,len(arr)-1,1))
+print(search(arr,0,len(arr)-1,2))
+print(search(arr,0,len(arr)-1,3))
+print(search(arr,0,len(arr)-1,4))
+print(search(arr,0,len(arr)-1,5))
+print(search(arr,0,len(arr)-1,6))
+print(search(arr,0,len(arr)-1,68))
