ff0c7f744ad7e23641e8862c05939aa586dfe4be
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-24 23:22:50 +0530

    ss

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,5 +1,9 @@
 N = 5
 
+def print_mat(mat):
+    for i in range(len(mat)):
+        print(mat[i])
+
 
 def findMaxValue(mat):
     maxValue = -100
@@ -9,23 +13,33 @@ def findMaxValue(mat):
     maxArr[N - 1][N - 1] = mat[N - 1][N - 1]
     maxv = mat[N - 1][N - 1]
 
+    # print(maxv)
+
     for j in range(N - 2, -1, -1):
         if mat[N - 1][j] > maxv:
             maxv = mat[N - 1][j]
         maxArr[N - 1][j] = maxv
+    
+    print_mat(maxArr)
+    
 
     maxv = mat[N - 1][N - 1]
 
     for i in range(N - 2, -1, -1):
         if mat[i][N - 1] > maxv:
             maxv = mat[i][N - 1]
-            maxArr[i][N - 1] = maxv
+        maxArr[i][N - 1] = maxv
+    print()
+    print_mat(maxArr)
+    print()
 
     for i in range(N - 2, -1, -1):
         for j in range(N - 2, -1, -1):
             if maxArr[i + 1][j + 1] - mat[i][j] > maxValue:
                 maxValue = maxArr[i + 1][j + 1] - mat[i][j]
             maxArr[i][j] = max(mat[i][j], max(maxArr[i][j + 1], maxArr[i + 1][j]))
+            print_mat(maxArr)
+            print()
 
     return maxValue
 
