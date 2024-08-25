b0feccf2444fae9e9b2e6815c748fbd4d79dc6e5
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-25 16:36:47 +0530

    orange problem start

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,30 +1,17 @@
-grid = [[1,2,3],
-        [8,9,4],
-        [7,6,5]]
+import collections
 
-row = len(grid)
-col = len(grid[0])
+grid = [[2,1,1],
+        [1,1,2],
+        [0,1,1]]
 
-up = 0
-down = row - 1
-left = 0
-right = col - 1
 
-while ( up <= down and left <= right):
-    for i in range(left,right + 1):
-        print(grid[up][i],end=" ")
-    up += 1
-    
-    for i in range(up,down+1):
-        print(grid[i][right],end=" ")
-    right -= 1
+visited=set()
+directions = [[1,0],[-1,0],[0,1],[0,-1]]
+q = collections.deque()
+row =len(grid)
+col =len(grid(0))
+fresh=set()
 
-    for i in range(right,left-1,-1):
-        print(grid[down][i],end=" ")
-    down -= 1
-    # print("Hl",down," ",up," hi",end="  ")
-
-    for j in range(down,up-1,-1):
-        # print("heelo","(",left,j,")",end="  ")
-        print(grid[j][left],end=" ")
-    left += 1
+for i in range(row):
+    for j in range(col):
+        
