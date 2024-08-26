86c9420ca56295483aa13a86e95dc26f61801319
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-26 09:11:57 +0530

    done prime

--- Competitive/Normal_tricky/prime_in_range.py
+++ Competitive/Normal_tricky/prime_in_range.py
@@ -1,7 +1,17 @@
 
 def all_prime_print(n):
     prime = [True for i in range(n)]
+    prime[0], prime[1] = False, False
     print(prime)
+    for i in range(2,n):
+        if(i*i > n):
+            break
+        if(prime[i]):
+            for j in range(i*i,n,i):
+                prime[j]=False
+    for k in range(2,n):
+        if(prime[k]):
+            print(k,end=" ")
 
 
 n = int(input())
