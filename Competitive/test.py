b0c014af3a5523481b7bdf794788cc329d1e2ddb
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-27 23:13:13 +0530

    bfs start

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,16 +1,4 @@
-grid = [1,2,3,4,4,7]
+grid =[[1,1,1,0,1],
+       [1,1,0,0,1],
+       [1,0,0,0,0]]
 
-low = 0
-high = len(grid)-1
-target = 4 
-ans=0
-
-while (low <= high):
-    mid = low + (high-low)//2
-    if(grid[mid] > target):
-        ans=mid
-        high = mid-1
-    else:
-        low =mid+1
-
-print(low)
