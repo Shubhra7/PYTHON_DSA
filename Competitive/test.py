6d0a693df62c83690e33d0d84d0b508fd221201a
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-30 21:06:55 +0530

    start

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,20 +1,7 @@
+def rebound_height(H,V,Vn):
+    
 
-def Score(arr):
-    count = 0
-    Score = 0
-    for i in range(len(arr)):
-       if (arr[i] == 'H'):
-            Score += 2
-            count += 1
-       else:
-           count=0
-           Score -= 1
-       if(count == 3):
-           return Score
-    return Score
-           
-arr = ['H','T','H','H','T','T','H','T','H','H','T']
-arr1 = ['H','T','H','H','T','T','H','T','H','H','H','T']
 
-print(Score(arr))
-print(Score(arr1))
+H, V, Vn = map(int,input().split())
+
+print(int(rebound_height(H,V,Vn)))
