03d44f99df96a6bb1ac5d21f9a3a6d703603480d
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-31 10:31:52 +0530

    done

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,19 +1,5 @@
-def paid_candy(n,arr,m):
-    total_candy=0
-    arr.sort()
-    for i in arr:
-        if( i%5 == 0):
-            total_candy += 1
-        else:
-            if(i <= m):
-                m -= i
-                total_candy += 1
-    return total_candy
+import math
+a=12
+b=5
 
-
-n=3
-arr=[5,15,105]
-m=8
-
-print(paid_candy(n,arr,m))
-print(paid_candy(4,[5,4,15,6],5))
+print(math.ceil((a**2 + b**2)**0.5))
