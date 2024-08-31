c04710888ef93b98f7f5e8e9d0286ab98f459751
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-31 21:18:25 +0530

    one done

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,22 +1,6 @@
-def sum1(n):
-    ans = 0
-    while (n>0):
-        rem = n % 10
-        ans += rem
-        n = n//10
-    return ans
-
-def pro(n):
-    ans = 1
-    while(n>0):
-        rem = n % 10
-        ans = ans * rem
-        n=n//10
-    return ans
-
 n=int(input())
-flag = 1
-if (n%2 == 0):
-    print(sum1(n))
-else:
-    print(pro(n))
+
+for i in range(n):
+    for j in range(n):
+        print("*",end="")
+    print()
