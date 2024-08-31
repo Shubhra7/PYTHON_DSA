dd5985ac8a0a33dc0d78b629603eeeaaa3570572
Shubrha <shubhrajit.g2002@gmail.com>
2024-08-31 21:22:03 +0530

    done

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,6 +1,12 @@
 n=int(input())
+for i in range(n):
+    print("*",end="")
+print()
+for j in range(n-2):
+    print('*',end="")
+    for i in range(n-2):
+        print(" ",end="")
+    print('*')
 
 for i in range(n):
-    for j in range(n):
-        print("*",end="")
-    print()
+    print("*",end="")
