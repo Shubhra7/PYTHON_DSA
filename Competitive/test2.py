d391f43f5c217916579bbebe10480bed4fb526e8
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-01 15:27:36 +0530

    done

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,28 +1,10 @@
-start = 3
+n = int(input("Enter the number: "))
 
-n = int(input("Enter the row: "))
-for i in range(1,n+1):
-    ans=""
-    for j in range(0,i):
-        ans = str(start+j) + ans
-    start += i
-    print(ans)
-
-pop = start
-start -= 1
-ans = ""
+k=1
 for i in range(n):
-    ans = ans + str(start)
-    start -= 1
-print(ans)
-
-
-count = pop - n - 1
-
-for i in range(n-1,-1,-1):
-    ans =""
-    for j in range(i,0,-1):
-        ans = ans + str(count)
-        count -= 1
-    print(ans)
-
+    for j in range(n):
+        print(k,end="")
+        if(j != n-1):
+            print("*",end="")
+        k += 1
+    print()
