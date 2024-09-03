fcd133d509d6cf750423cac35b780c11ac22cb25
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-03 10:19:06 +0530

    done

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,8 +1,17 @@
 import re
-input1 = "abc   bcd cda"
+input1 = "abc bcd cda"
 input2 = "abc bad"
 
 # ans = re.split(r'\s+',input1)
-ans = re.sub('[^ab]','#',input2)
-print(ans)
-print(len(ans))
+ans1, ans2 = 0,0
+for i in input1:
+    if(i==' '):
+        ans1 += 1
+for i in input2:
+    if(i==' '):
+        ans2 += 1
+    
+if (abs(ans2 - ans1) % 2==0):
+    print('Even:',abs(ans1-ans2))
+else:
+    print('Odd:',abs(ans1-ans2))
