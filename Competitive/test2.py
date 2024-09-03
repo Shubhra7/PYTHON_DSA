51886e93fa52e14877dc1280c167befb1907f063
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-03 09:15:37 +0530

    Yes

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,13 +1,14 @@
+def max_version(arr,n):
+    maxi = -1
+    for i in range(len(arr)):
+        l=list(arr[i].split('_'))
+        if(len(l)==2 and l[1].isdigit() and l[0]=='File'):
+            maxi=max(maxi, int(l[1]))
+    return maxi
+
 n=4
-# arr = ["File_2","File_1","File_6s","Fil_7"]
-arr = ["File_2s","File_1s","File_6s","Fil_7"]
-maxi = -1
-p=""
-for i in range(len(arr)):
-    l=list(arr[i].split('_'))
-    if(len(l)==2 and l[1].isdigit() and l[0]=='File'):
-        maxi=max(maxi, int(l[1]))
-        p = "_".join(l)
-    
-print(maxi)
-print(p)
+arr1 = ["File_2","File_1","File_6","Fil_7"]
+print(max_version(arr1,n))
+arr = ["File_2","File_1","File_6s","Fil_7"]
+print(max_version(arr,n))
+
