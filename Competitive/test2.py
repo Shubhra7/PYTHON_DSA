0a2ab69f0678e86266f7e034e45e217935daee70
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-03 09:06:39 +0530

    ss

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,6 +1,8 @@
 n=4
-arr = ["File_2","File_1","File_6","Fil_7"]
+# arr = ["File_2","File_1","File_6s","Fil_7"]
+arr = ["File_2s","File_1s","File_6s","Fil_7"]
 maxi = -1
+p=""
 for i in range(len(arr)):
     l=list(arr[i].split('_'))
     if(len(l)==2 and l[1].isdigit() and l[0]=='File'):
