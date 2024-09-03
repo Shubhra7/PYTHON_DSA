15e7a765362e821ded7a442638b4fa0357fab4e9
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-03 09:27:07 +0530

    done

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,14 +1,6 @@
-def max_version(arr,n):
-    maxi = -1
-    for i in range(len(arr)):
-        l=list(arr[i].split('_'))
-        if(len(l)==2 and l[1].isdigit() and l[0]=='File'):
-            maxi=max(maxi, int(l[1]))
-    return maxi
+n=int(input())
 
-n=4
-arr1 = ["File_2","File_1","File_6","Fil_7"]
-print(max_version(arr1,n))
-arr = ["File_2","File_1","File_6s","Fil_7"]
-print(max_version(arr,n))
-
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
