304647b6edacf32912b7113e6eebfe0547b05e59
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-03 08:57:54 +0530

    done with python

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,5 +1,11 @@
 n=4
-arr = ["File_2","File_1","File_6","Fil_3"]
-
+arr = ["File_2","File_1","File_6","Fil_7"]
+maxi = -1
 for i in range(len(arr)):
+    l=list(arr[i].split('_'))
+    if(len(l)==2 and l[1].isdigit() and l[0]=='File'):
+        maxi=max(maxi, int(l[1]))
+        p = "_".join(l)
     
+print(maxi)
+print(p)
