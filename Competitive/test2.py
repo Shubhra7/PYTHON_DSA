9a3173ad0fda5a138a7115209ac9a0e8fa3314be
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-03 09:34:58 +0530

    done

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,6 +1,10 @@
-n=int(input())
+arr = [1,2,3,6,10,11,13,21,5,4]
 
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        print(j,end=" ")
-    print()
+even, odd = 0,0
+for i in arr:
+    if(i%2==0):
+        even += i
+    else:
+        odd += i
+
+print(even,"  ",odd)
