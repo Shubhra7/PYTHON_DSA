5e4ad945e978279a6ee44052ab605220b7c8abca
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-05 11:52:52 +0530

    done

---
+++ Competitive/test2.py
@@ -0,0 +1,11 @@
+def add_xor(arr):
+    ans1, ans2= arr[0],arr[1]
+    for i in range(2,len(arr)):
+        if(i%2==0):
+            ans1 += arr[i]
+        else:
+            ans2 ^= arr[i]
+    return ans1 + ans2
+            
+arr = [1,2,4,2,1]
+print(add_xor(arr))
