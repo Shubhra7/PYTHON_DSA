488622059272e82483847f64317b1527e7fe9d53
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-05 11:41:27 +0530

    done

---
+++ Competitive/test2.py
@@ -0,0 +1,15 @@
+
+def special_fibo(n):
+    if(n==0 or n==1):
+        return 1
+    a=1
+    b=1
+    for i in range(2,n+1):
+        c = (a**2) + (b**2)
+        a=b
+        b=c
+    return (b%47)
+
+
+n=int(input("Enter the value of n: "))
+print(special_fibo(n))    
