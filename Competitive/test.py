eeadf4375e3c23bc29425db4b736608c9e4b817a
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-05 13:48:35 +0530

    hm

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,40 +1,26 @@
-def solve(n,e,d):
-    if(n==0):
-        return -1
-    if(e==0 or d==0):
-        return 0
-    if(d<7):
-        if((d*e)%n == 0):
-            return (d*e)//n
-        else:
-            return ((d*e)//n + 1)
-    if((n*6) < (e*7)):
-        return -1
-    
-    ans = 0
-    weeks = d // 7
-    if((weeks*e*7)%n == 0):
-        ans += (weeks*e*7)//n
-    else:
-        ans += (((weeks*e*7)//n)+1)
- 
-    left = (ans*n) - (weeks*e*7)
+text = "hello.hubhrajit.name.is.shubhrajit"
 
-    left_days = d%7
+count = 0
+ans = ""
+maxi=0
+j=0
+for i in range(len(text)):
+    if(text[i] == '.'):
+        print(i," ",j)
+        val = (i-j)
+        if(val > maxi):
+            maxi = val
+            ans = text[j:i]
+        j=i+1
+print(i," ",j)
+val = (i-j)+1   # beacuse in python the i will stop at last value not exceed 
+print(val)
+if(val > maxi):
+    maxi=val
+    ans = text[j:i+1]
 
-    req = (left_days*e) - left
-
-    if(req % n == 0):
-        ans += (req//n)
-    else:
-        ans += ((req//n)+1)
- 
-    return ans
+print(maxi)
+print(ans)
 
 
 
-n=5
-e=2
-d=10
-
-print(solve(n,e,d))
