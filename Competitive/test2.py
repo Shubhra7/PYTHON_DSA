d84844c3a2fba60b9dfe478f3d2dc9b1db6bd4d8
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-07 21:45:34 +0530

    done

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,26 +1,9 @@
-def fact(n):
-    if(n==1):
-        return 1
-    else:
-        val = 1
-        for i in range(1,n+1):
-            val *= i 
-        return val
+pass1 = "File_12"
 
-text = "abbcep"
-l = ['a','e','i','o','u','A','E','I','O','U']
-d={}
-Ccount = 0
+l=list(pass1.split('_'))
 
-for i in text:
-    if i not in l:
-        d[i]=d.get(i,0)+1
-        Ccount += 1
-
-req = 1
-for i in d.values():
-    if (i>1):
-        req = req * fact(i)
-
-print(Ccount)
-print("The permutaion result is: ",fact(Ccount)/req)
+if (l[0]=="File"):
+    if(l[1].isdigit()):
+        print(int(l[1]))
+else:
+    print(-1)
