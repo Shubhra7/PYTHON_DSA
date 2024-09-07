a9a15ce5f5de2351cea1159a12cfdb42fbd528ce
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-07 22:29:11 +0530

    dibe

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,9 +1,37 @@
 
-l = [1,2,3,4,1,2,0,1,2,3]
-print(len(l))
+def solve(n,e,d):
+    if (n==0):
+        return -1
+    if(e==0 or d==0):
+        return 0
+    if(d<7):
+        if((d*e)%n == 0):
+            return (d*e)//n
+        else:
+            return ((d*e)//n + 1)
+    if((n*6) < (e*7)):
+        return -1
+    
+    ans = 0
+    weeks = d//7
+    if((weeks*e*7)%n == 0):
+        ans += (weeks*e*7)//n
+    else:
+        ans += (((weeks*e*7)//n)+1)
+    
+    left = (ans * n) - (weeks*e*7)
+    left_days = d%7
 
-sum = 0
-for i in range(0,len(l),2):
-    sum += l[i]
+    req = (left_days*e) - left
 
-print(sum)
+    if(req % n==0):
+        ans += (req//n)
+    else:
+        ans += (req//n)+1
+    
+    return ans
+
+
+n,e,d = 5,2,10
+
+print(solve(n,e,d))
