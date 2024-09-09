424bbb262a42b6e02cd9906c811793ec92cf2bf0
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-09 10:51:01 +0530

    done

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,4 +1,21 @@
-n=int(input("Enter the radius value: "))
+# arr=[1,6,0,-1,-2,3,4,8,6,4,3,2,1,0]
+arr=[1,0]
 
-ans = 3.14*n*n
-print(round(ans))
+count=0
+maxi=0
+j=0
+p=[]
+for i in range(1,len(arr)):
+    if(arr[i]<arr[i-1]):
+        if arr[i-1] not in p:
+            p.append(arr[i-1])
+        p.append(arr[i])
+        count += 1
+        maxi = max(maxi,count+1)
+    else:
+        count = 0
+        j=i
+
+print(maxi)
+print(p[-maxi:len(p)])
+
