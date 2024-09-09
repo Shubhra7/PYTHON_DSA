8337d98a9d2413e6d31c74c3c491b30f07b7d0c1
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-09 12:15:58 +0530

    python power done

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,19 +1,3 @@
-import sys
-# arr = [5,4,2,1,-2,1,3]
-arr=[3,3]
+n=14
 
-min, min2 = sys.maxsize, sys.maxsize
-
-for i in range(len(arr)):
-    if(min > arr[i]):
-        min2 = min
-        min = arr[i]
-    elif(min2 > arr[i] and min!=arr[i]): # 2nd condition for avoid duplicate values
-        min2=arr[i]
-
-if(min2 == sys.maxsize):
-    print("Not possible")
-else:
-    print(min2)
-    
-
+print(bin(n)[2:].count('1'))
