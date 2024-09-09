8849360c6156fe090580fcfd7e65dee66c107710
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-09 12:51:47 +0530

    done

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,15 +1,23 @@
-text = "TeamATeamBTeamATeamBTeamATeamBTeamBTeamA"
- 
-i=4 
-Aval, Bval = 0,0
-while (i < len(text)):
-    if(text[i]=='A'):
-        Aval += 1
-    else:
-        Bval += 1
-    i += 5
+def rev(arr):
+    return arr[::-1]
 
-if(Aval > Bval):
-    print("TeamA")
-else:
-    print("TeamB")
+arr = [1,2,3,4,5]
+
+# Method: 1
+s=[0 for i in range(len(arr))]
+k=2
+n=len(arr)
+
+for i in range(len(arr)):
+    s[(i+k)%n]=arr[i]
+
+print(s)
+
+# Method: 2
+# First reverse all
+# reverse first k elements
+# reverse lask n-k elements
+k=2
+arr=arr[::-1]
+s1=rev(arr[:k])+(rev(arr[k:]))
+print(s1)
