2463195a23df5e3e6217535bc9406a17c9b24389
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-09 11:46:22 +0530

    done

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,5 +1,5 @@
 arr=[1,6,0,-1,-2,3,4,8,6,4,3,2,1,0]
-# arr=[1,0]
+# arr=[1]
 
 count=0
 maxi=0
@@ -13,11 +13,10 @@ for i in range(1,len(arr)):
         count += 1
         # maxi = max(maxi,count+1)
     else:
-        print(maxi,"  ",count+1)
         maxi = max(maxi,count+1)
         count = 0
         j=i
-    maxi=max(maxi,count+1)
+    maxi=max(maxi,count+1)   #checking for last one also
 
 print(maxi)
 print(p[-maxi:len(p)])
