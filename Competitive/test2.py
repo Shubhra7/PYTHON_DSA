bc2273ec987376d50932ef27fd264e52f2787deb
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-09 11:52:08 +0530

    done

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,23 +1,8 @@
-arr=[1,6,0,-1,-2,3,4,8,6,4,3,2,1,0]
-# arr=[1]
+n=int(input())
 
-count=0
-maxi=0
-j=0
-p=[]
-for i in range(1,len(arr)):
-    if(arr[i]<arr[i-1]):
-        if arr[i-1] not in p:
-            p.append(arr[i-1])
-        p.append(arr[i])
-        count += 1
-        # maxi = max(maxi,count+1)
-    else:
-        maxi = max(maxi,count+1)
-        count = 0
-        j=i
-    maxi=max(maxi,count+1)   #checking for last one also
+val = 3.14*n*n
 
-print(maxi)
-print(p[-maxi:len(p)])
-
+if( val - int(val) > 0.5):
+    print(int(val)+1)
+else:
+    print(int(val))
