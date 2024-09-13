6c8789bde306e1cef922e7df2e56f6ef092c024d
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-13 14:20:39 +0530

    kadane done

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,26 +1,10 @@
-import sys
-def min_hour_consume(arr,h):
-    low=1
-    high=max(arr)
-    ans=sys.maxsize
-    while(low<=high):
-        mid = low + (high-low)//2
-        req=0
-        for i in arr:
-            req += (i//mid) + (i%mid!=0)
-        print(mid," : ",req)
-        if req <= h:
-            ans=mid
-            high=mid-1
-            print("Yes")
-        else:
-            low = mid+1
+def maxSubarraySum(arr,n):
+    cur_max=arr[0]
+    ans=0
+    for i in range(1,len(arr)):
+        cur_max = max(arr[i], cur_max + arr[i])
+        ans = max(cur_max,ans)
     return ans
 
-arr = [4,9,11,17]
-
-h=8
-
-ans = min_hour_consume(arr,h)
-print(ans)
-
+arr=[5,4,-1,-17,8]
+print(maxSubarraySum(arr,10))
