a0a0ff90d5370b98101d310bca374a572db6dc23
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-13 14:30:19 +0530

    done basic dp

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,10 +1,28 @@
-def maxSubarraySum(arr,n):
-    cur_max=arr[0]
-    ans=0
-    for i in range(1,len(arr)):
-        cur_max = max(arr[i], cur_max + arr[i])
-        ans = max(cur_max,ans)
-    return ans
+s1="adventure"
+s2="future"
 
-arr=[5,4,-1,-17,8]
-print(maxSubarraySum(arr,10))
+row,col =len(s1), len(s2)
+dp=[[0 for i in range(len(s2)+1)] for j in range(len(s1)+1)]
+maxi=-1
+ro,co =-1,-1
+
+for i in range(1,row+1):
+    for j in range(1,col+1):
+        if(s1[i-1] == s2[j-1]):
+            dp[i][j]=dp[i-1][j-1]+1
+            if(dp[i][j] > maxi):
+                maxi=dp[i][j]
+                ro,co=i,j
+        else:
+            dp[i][j]=0
+
+print(maxi)
+
+ans=""
+for i in range(maxi):
+    ans =s1[ro-1] + ans
+    ro -= 1
+
+print(ans)
+
+
