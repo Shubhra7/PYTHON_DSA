6d133cbf88af1bdbed25ac3658c78eaca14c1e74
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-15 12:56:10 +0530

    counter gives default 0

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,73 +1,7 @@
-# grid=["*.*#***",
-#       "*.*#*.*",
-#       "***#***"]
-# n=7
-# -------------------------------
-# grid=["*.*#***#***.*.",
-#       "*.*#*.*#******",
-#       "***#***#****.*"]
+from collections import Counter
 
-# n=14
-# ----------------------------------------
-# grid=["*.*#***#***#***.*.",
-#       "*.*#*.*#.*.#******",
-#       "***#***#***#****.*"]
+nums=[1,2,1,4]
 
-# n=18   U#O#I#EA
-# --------------------------------------------
-
-#   12
-
-#   * . * # . * * * # . * .
-
-#   * . * # . . * . # * * *
-
-#   * * * # . * * * # * . *
-
-#   Output
-
-#   U#I#A
-
-n=int(input())
-co=[]
-for i in range(3):
-    co.append(list(input().split()))
-
-print("The ceo is: ")
-for i in co:
-    print(i)
-
-print()
-i=0
-while i<n-2:    
-    if co[0][i]=='#':       
-         print("#",end="")        
-         i+=1        
-         continue   
-    if co[0][i]=='.' and co[0][i+1]=='*' and co[0][i+2]=='.':
-          if co[1][i]=='*' and co[1][i+1]=='*' and co[1][i+2]=='*':            
-              if co[2][i]=='*' and co[2][i+1]=='.' and co[2][i+2]=='*':                
-                  print('A',end="")    
-    if co[0][i]=='*' and co[0][i+1]=='*' and co[0][i+2]=='*':        
-        if co[1][i]=='*' and co[1][i+1]=='*' and co[1][i+2]=='*':            
-            if co[2][i]=='*' and co[2][i+1]=='*' and co[2][i+2]=='*':                
-                print('E',end="")                
-                i+=3                
-                continue        
-        elif co[1][i]=='.' and co[1][i+1]=='*' and co[1][i+2]=='.':            
-                if co[2][i]=='*' and co[2][i+1]=='*' and co[2][i+2]=='*':                
-                    print('I',end="")                
-                    i+=3                
-                    continue        
-        elif co[1][i]=='*' and co[1][i+1]=='.' and co[1][i+2]=='*':            
-            if co[2][i]=='*' and co[2][i+1]=='*' and co[2][i+2]=='*':                
-                print('O',end="")                
-                i+=3               
-                continue    
-    if co[0][i]=='*' and co[0][i+1]=='.' and co[0][i+2]=='*':        
-        if co[1][i]=='*' and co[1][i+1]=='.' and co[1][i+2]=='*':            
-            if co[2][i]=='*' and co[2][i+1]=='*' and co[2][i+2]=='*':                
-                print('U',end="")                
-                i+=3                
-                continue    
-    i+=1
+ans=Counter(nums)
+print(ans)
+print(ans[5])
