3492a283c4be3de44d0ba3f5ea65e74b78a559e9
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-17 00:39:43 +0530

    java done

---
+++ Codevita/Sieve_of_Erathosthenes.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Sieve_of_Erathosthenes{
+    static boolean isPrime(int n){
+        if(n<=1){
+            return false;
+        }
+        if(n<=3){
+            return true;
+        }
+        if(n%2==0 || n%3==0){
+            return false;
+        }
+        int i=5;
+        while (i*i <= n) {
+            if(n%i==0 || n%(i+2)==0){
+                return false;
+            }
+        i += 6;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println("HI bubai");
+        System.out.println("16: "+isPrime(16));
+        System.out.println("17: "+isPrime(17));
+    }
+}
