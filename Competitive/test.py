ef66f177027063ebed083a0b46920546f6ecc3c2
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-16 23:59:16 +0530

    check done

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,19 +1,27 @@
 import math
 
+# def isPrime(n):
+#     """Check if a number n is a prime number."""
+#     if n <= 1:
+#         return False
+#     if n <= 3:
+#         return True
+#     if n % 2 == 0 or n % 3 == 0:
+#         return False
+#     i = 5
+#     while i * i <= n:
+#         if n % i == 0 or n % (i + 2) == 0:
+#             return False
+#         i += 6
+#     return True
+
 def isPrime(n):
-    """Check if a number n is a prime number."""
-    if n <= 1:
-        return False
-    if n <= 3:
+    if n<=3:
         return True
-    if n % 2 == 0 or n % 3 == 0:
-        return False
-    i = 5
-    while i * i <= n:
-        if n % i == 0 or n % (i + 2) == 0:
-            return False
-        i += 6
-    return True
+    else:
+        if( ((n**2)-1)%24==0):
+            return True
+    return False
 
 def checkPrime(l, ad):
     """Check if all numbers in the list l incremented by ad are prime."""
@@ -41,7 +49,7 @@ print("Modified list l:", l)
 # Check prime status for various ad values
 count = 0
 print()
-for i in range(n):
+for i in range(1,n):
     if checkPrime(l, i):
         count += 1
         print(f"List l with ad = {i}: {[x + i for x in l]}")
