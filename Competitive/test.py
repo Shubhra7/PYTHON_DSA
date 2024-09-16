80f305956a1d19a01fcc6440edf41e574b7664b8
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-16 23:47:05 +0530

    done

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,22 +1,49 @@
-def checkPrime(l,i):
-    for i in range(len(l)):
-        p=
+import math
 
-d = int(input())
-p = int(input())
+def isPrime(n):
+    """Check if a number n is a prime number."""
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
 
-n = d//p
+def checkPrime(l, ad):
+    """Check if all numbers in the list l incremented by ad are prime."""
+    pi = [x + ad for x in l]
+    return all(isPrime(x) for x in pi)
 
-l=[0 for i in range(p)]
-print(l)
+# Read inputs
+d = int(input("Enter d: "))
+p = int(input("Enter p: "))
 
-val=1
+n = d // p
+
+# Initialize list l
+l = [0] * p
+print("Initial list l:", l)
+
+# Modify the list l
+val = 1
 for i in range(p):
     l[i] += val
     val += n
 
-print(l)
+print("Modified list l:", l)
 
-val=1
+# Check prime status for various ad values
+count = 0
+print()
 for i in range(n):
-    checkPrime(l,i)
+    if checkPrime(l, i):
+        count += 1
+        print(f"List l with ad = {i}: {[x + i for x in l]}")
+
+print("Count of valid ad values:", count)
