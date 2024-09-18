8339ea0b5c6a0eef84708df409544bd3720717e9
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-18 10:02:11 +0530

    2+

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,57 +1,22 @@
-import math
+def target(n):
+    ans=((n//60 + 1)*60)
+    return ans
 
-# def isPrime(n):
-#     """Check if a number n is a prime number."""
-#     if n <= 1:
-#         return False
-#     if n <= 3:
-#         return True
-#     if n % 2 == 0 or n % 3 == 0:
-#         return False
-#     i = 5
-#     while i * i <= n:
-#         if n % i == 0 or n % (i + 2) == 0:
-#             return False
-#         i += 6
-#     return True
-
-def isPrime(n):
-    if n<=3:
-        return True
-    else:
-        if( ((n**2)-1)%24==0):
-            return True
-    return False
-
-def checkPrime(l, ad):
-    """Check if all numbers in the list l incremented by ad are prime."""
-    pi = [x + ad for x in l]
-    return all(isPrime(x) for x in pi)
-
-# Read inputs
-d = int(input("Enter d: "))
-p = int(input("Enter p: "))
-
-n = d // p
-
-# Initialize list l
-l = [0] * p
-print("Initial list l:", l)
-
-# Modify the list l
-val = 1
-for i in range(p):
-    l[i] += val
-    val += n
-
-print("Modified list l:", l)
-
-# Check prime status for various ad values
-count = 0
-print()
-for i in range(1,n):
-    if checkPrime(l, i):
+arr = [2,58,58,2,60,60]
+d={}
+l=[]
+count=0
+for i in range(len(arr)):
+    req = target(arr[i]) - arr[i]
+    if(req in d.keys() and d[req]>0):
         count += 1
-        print(f"List l with ad = {i}: {[x + i for x in l]}")
+        d[req] -= 1
+        l.append([arr[i],req])
+    else:
+        d[arr[i]] = d.get(arr[i],0)+1
+print(count)
+print("The pairs are: ")
+print(l)
 
-print("Count of valid ad values:", count)
+
+    
