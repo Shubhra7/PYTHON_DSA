7f55a270cb90f3b2f17b294d4d6094981f93b30f
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-19 21:13:50 +0530

    single linkedlist reverse done

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,8 +1,41 @@
-def win_joshep(n,k):
-    winner = 1
-    for i in range(2,n+1):
-        winner = (winner + (k-1))%i + 1
-    # print(winner)
-    return winner
+class Node:
+    def __init__(self,data=None, link=None):
+        self.data=data
+        self.link=link
 
-print(win_joshep(41,2))
+class SLL:
+    def __init__(self):
+        self.start = None
+    def add_first(self,data):
+        n=Node(data)
+        n.link=self.start
+        self.start=n
+        print("hi")
+    def display(self):
+        p=self.start
+        while p is not None:
+            print("|",p.data,"|",end=" -> ")
+            p=p.link
+    def reverse(self):
+        l=None
+        q=self.start
+        n=q.link
+        while q.link is not None:
+            q.link=l
+            l=q
+            q=n
+            n=q.link
+        q.link=l
+        self.start=q
+
+obj=SLL()
+obj.add_first(4)
+obj.add_first(3)
+obj.add_first(5)
+obj.add_first(52)
+
+obj.display()
+obj.reverse()
+print()
+obj.display()
+        
