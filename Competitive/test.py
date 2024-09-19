5e2e9e22b8aaed8a30f62cffdbfdf980cbedce98
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-19 20:29:31 +0530

    change hota hain

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,22 +1,7 @@
-def target(n):
-    ans=((n//60 + 1)*60)
-    return ans
+def kalu(l):
+    l.pop()
+    return 1
 
-arr = [2,58,58,2,60,60]
-d={}
-l=[]
-count=0
-for i in range(len(arr)):
-    req = target(arr[i]) - arr[i]
-    if(req in d.keys() and d[req]>0):
-        count += 1
-        d[req] -= 1
-        l.append([arr[i],req])
-    else:
-        d[arr[i]] = d.get(arr[i],0)+1
-print(count)
-print("The pairs are: ")
+l=[2,3,4,6]
+ans=kalu(l)
 print(l)
-
-
-    
