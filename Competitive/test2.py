935fd451b591585485a402538f6057ad2298d71a
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-19 21:51:42 +0530

    power of python

--- Competitive/test2.py
+++ Competitive/test2.py
@@ -1,41 +1,4 @@
-class Node:
-    def __init__(self,data=None, link=None):
-        self.data=data
-        self.link=link
+d=[2,4,6,7,8]
 
-class SLL:
-    def __init__(self):
-        self.start = None
-    def add_first(self,data):
-        n=Node(data)
-        n.link=self.start
-        self.start=n
-        print("hi")
-    def display(self):
-        p=self.start
-        while p is not None:
-            print("|",p.data,"|",end=" -> ")
-            p=p.link
-    def reverse(self):
-        l=None
-        q=self.start
-        n=q.link
-        while q.link is not None:
-            q.link=l
-            l=q
-            q=n
-            n=q.link
-        q.link=l
-        self.start=q
-
-obj=SLL()
-obj.add_first(4)
-obj.add_first(3)
-obj.add_first(5)
-obj.add_first(52)
-
-obj.display()
-obj.reverse()
-print()
-obj.display()
-        
+ans = all(x%2==0 for x in d)
+print(ans)
