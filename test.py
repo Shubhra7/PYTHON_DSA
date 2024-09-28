7b1eed0e8c077634c21f99041bf538162545af6f
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-28 09:55:25 +0530

    start

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
+import collections
+
+q= collections.deque()
+visited = set()
+direction = [[1,0],[-1,0],[0,1],[0,-1]]
+
 grid = [[1,1,0,0,1],
         [1,1,0,0,1],
         [1,0,0,0,0]]
 
-row = 
+row = len(grid)
+col = len(grid[0])
+
+count = 0
+for i in range(row):
+    for j in range(col):
+        if(grid[i][j]==1):
+            count += 1
+            bfs(i,j)
