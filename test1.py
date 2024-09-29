21a7571ece3668fc9a883e2fc6295c460b093b6c
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-29 12:23:40 +0530

    done

--- test1.py
+++ test1.py
@@ -1,13 +1,1 @@
-no1=list(map(int,input()))
-no2=list(map(int,input()))
-no3=list(map(int,input()))
-
-l=[no1,no2,no3]
-print(l)
-
-ans=1
-ans=min([i[2]] for i in l)[0]
-ans=ans + min([i[1]] for i in l)[0]*10
-ans=ans + min([i[0]] for i in l)[0]*100
-ans=ans + max([max(no1),max(no2),max(no3)])*1000
-print(ans)
+print('Hi debjit')
