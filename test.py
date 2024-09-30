9b7272419ebc66d09eefb2fd772f36e9bb115b47
Shubrha <shubhrajit.g2002@gmail.com>
2024-09-30 17:14:03 +0530

    accenture anargram check done

--- test.py
+++ test.py
@@ -1,34 +1,24 @@
-text1 = 'adventureabba'
-text2 = 'fventuture'
+from collections import Counter
 
-lt1, lt2 =len(text1), len(text2)
+def anargram_check(s1,s2):
+    p=Counter(s1)
+    print(p)
+    for i in s2:
+        if(i not in p.keys()):
+            return False
+        p[i] = p.get(i)  - 1
 
-dp = [ [0 for i in range(lt1+1)] for j in range(lt2+1)]
-maxi=0
-ro,co = -1,-1
+    print(p)
+    ans = 0
+    for i in p.values():
+        ans += i
 
-for i in range(1,lt2+1):
-    for j in range(1,lt1+1):
-        if(text2[i-1]==text1[j-1]):
-            print(text2[i-1])
-            dp[i][j] = dp[i-1][j-1]+1
-            if(dp[i][j] > maxi):
-                print(i," : ",j)
-                maxi=dp[i][j]
-                ro,co = i,j
-        else:
-            dp[i][j]=max(dp[i][j-1], dp[i-1][j])
+    if(ans == 0):
+        return True
+    else:
+        return False
 
-for i in dp:
-    print(i)
-
-print("The maximum substring is length of: ",maxi)
-
-i=0
-ans=""
-while i<maxi:
-    ans = text2[ro-1] + ans
-    ro -=1
-    i+=1
-print(ans)
+s1 = "abbkalllu"
+s2="bbkallalu"
+print(anargram_check(s1,s2))
 
