dfa7f9a0605f8461ec59f76953bbec3bdf44d737
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-02 13:55:40 +0530

    done

--- Tree/test.py
+++ Tree/test.py
@@ -2,17 +2,23 @@ from collections import deque
 
 q= deque()
 visited = set()
-directions = [[1,0],[0,1],[-1,0],[0]]
+directions = [[1,0],[0,1],[-1,0],[0,-1],[1,1],[-1,-1]]
 
 def bfs(i,j):
     q.append((i,j))
     visited.add((i,j))
     while q:
         ro, co = q.popleft()
+        for m,n in directions:
+            nro, ncol = ro+m, co+n
+            if(nro in range(row) and ncol in range(col) and 
+               grid[nro][ncol]==1 and (nro,ncol) not in visited):
+                visited.add((nro,ncol))
+                q.append((nro,ncol))
 
 grid = [[1,1,1,0,1,0,1],
         [1,1,0,0,1,0,1],
-        [1,0,0,1,1,0,0]]
+        [1,0,1,1,1,0,0]]
 
 row = len(grid)
 col = len(grid[0])
@@ -20,7 +26,7 @@ cnt =0
 
 for i in range(row):
     for j in range(col):
-        if(grid[i][j]==1):
+        if(grid[i][j]==1 and (i,j) not in visited):
             bfs(i,j)
             cnt += 1
 
