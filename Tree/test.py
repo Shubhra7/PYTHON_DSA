b87c2e1918232cae6362aa92735bd679171a86c1
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-02 13:47:07 +0530

    done

--- Tree/test.py
+++ Tree/test.py
@@ -1,14 +1,4 @@
+grid = [[1,0,1],
+        [0,1,0],
+        [1,0,1]]
 
-def maxSubarraySum(arr,n):
-    ans = 0
-    cur_max=arr[0]
-    for i in range(1,len(arr)):
-        cur_max=max(arr[i],arr[i]+cur_max)
-        ans=max(cur_max,ans)
-    return ans
-
-
-arr=[5,4,-1,7,8]
-print(maxSubarraySum(arr,10))
-arr1=[5]
-print(maxSubarraySum(arr1,10))
