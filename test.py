c0bc6c2bea07d269d17699dc5af24db1718993b1
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-04 19:11:50 +0530

    building logics

--- test.py
+++ test.py
@@ -13,4 +13,24 @@ for i in premap:
 
 flag = [False for i in range(n)]
 
+for i in range(0,len(pairs)):
+    a= pairs[i][0]
+    b= pairs[i][1]
+    prefB = premap[a][b]
+    prefA = premap[b][a]
+    for j in range(i+1,len(pairs)):
+        ele1 = pairs[j][0]
+        ele2 = pairs[j][1]
+        prefEle2 = premap[ele1][ele2]
+        prefEle1 = premap[ele2][ele1]
+
+        if(prefB > premap[a][ele1] and prefEle2 > premap[ele1][a]):
+            flag[a]= True
+            flag[ele1]= True
+
+        if(prefB > premap[a][ele2] and prefEle1 > premap[ele2][a]):
+            flag[a]=True
+            flag[ele2] = True
+        
+        
 
