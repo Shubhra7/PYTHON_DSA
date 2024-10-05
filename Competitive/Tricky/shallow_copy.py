be0a76f14b01c87ac8c02bb211a4440ceffa28b9
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-05 11:58:45 +0530

    done

--- Competitive/Tricky/shallow_copy.py
+++ Competitive/Tricky/shallow_copy.py
@@ -1,7 +1,7 @@
 import copy
 
 old_list = [[1,2],[3,4],[5,6,7]]
-new_list = copy.copy(old_list)
+new_list = copy.copy(old_list)   # for shallow copy "copy" used 
 
 # If append something 
 old_list.append([9,9,9,9,9])
@@ -14,3 +14,9 @@ print(new_list)
 # If make some changes in existing things
 
 old_list[1][1] = 69
+
+print("After making changes in existing list : ")
+print(old_list)
+print(new_list)
+print("In both are changed!!!")
+
