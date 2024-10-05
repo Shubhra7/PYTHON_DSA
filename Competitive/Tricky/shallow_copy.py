c63f7d15b534e6884538918adc18355f0886a0f0
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-05 11:56:33 +0530

    shallow copy start

---
+++ Competitive/Tricky/shallow_copy.py
@@ -0,0 +1,16 @@
+import copy
+
+old_list = [[1,2],[3,4],[5,6,7]]
+new_list = copy.copy(old_list)
+
+# If append something 
+old_list.append([9,9,9,9,9])
+
+print("After appending: ")
+print(old_list)
+print(new_list)
+
+
+# If make some changes in existing things
+
+old_list[1][1] = 69
