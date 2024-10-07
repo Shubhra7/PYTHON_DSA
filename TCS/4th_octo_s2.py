80fd7986c279830ef95c7df9983103d40fa646a4
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-07 18:14:04 +0530

    done

---
+++ TCS/4th_octo_s2.py
@@ -0,0 +1,20 @@
+# You are given an integer arry nums. You are initially positioned at the array's first
+# index, and each element in the array represents your maximum jump length at that position.
+# Return true if you can reach the last index, or false otherwise. 
+
+# i/p = [2,3,1,1,4]
+# o/p = True
+
+# i/p = [3,2,1,0,4]
+# o/p = False
+
+def check(arr):
+    maxi=0
+    for i in range(len(arr)):
+        if i>maxi:
+            return False
+        maxi = max(maxi, i+arr[i])
+    return True
+
+arr = list(map(int,input().split(',')))
+print("Answer is: ",check(arr))
