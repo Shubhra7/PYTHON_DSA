e7dd953bb267080cef2fb19744fc17ba36f3c71a
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-07 09:51:51 +0530

    done

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,14 +1,13 @@
 def dfs(i,visited,ans):
     visited.add(i)
+    ans.append(i)
     for j in adj[i]:
         if(j not in visited):
-            ans.append(j)
             dfs(j,visited,ans)
 
 adj = [[1,2],[3,4],[5],[6],[],[],[]]
 
 visited=set()
 ans=[]
-for i in range(7):
-    dfs(i,visited,ans)
+dfs(0,visited,ans)
 print(ans)
