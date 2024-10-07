a3c68b8ae52163ea0839f6eda5c95c08f8a34d3b
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-07 10:03:11 +0530

    done

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,3 +1,23 @@
+from collections import deque
+
+def bfs(start,visited,ans,adj):
+    q = deque()
+    q.append(start)
+    ans.append(start)
+    visited.add(start)
+    while q:
+        item = q.popleft()
+        for i in adj[item]:
+            if(i not in visited):
+                q.append(i)
+                visited.add(i)
+                ans.append(i)
+        
 
 adj = [[1,2],[3,4],[5],[6],[],[],[]]
+
+ans =[]
+visited = set()
+bfs(0,visited,ans,adj)
+print(ans)
 
