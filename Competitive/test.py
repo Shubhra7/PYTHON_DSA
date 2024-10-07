ad3af107d0005f44eecf5fd1ac85a1585b424c3f
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-07 09:54:31 +0530

    bfs testing start

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,13 +1,3 @@
-def dfs(i,visited,ans):
-    visited.add(i)
-    ans.append(i)
-    for j in adj[i]:
-        if(j not in visited):
-            dfs(j,visited,ans)
 
 adj = [[1,2],[3,4],[5],[6],[],[],[]]
 
-visited=set()
-ans=[]
-dfs(0,visited,ans)
-print(ans)
