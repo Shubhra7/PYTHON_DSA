1c4e50ea445e446901325e441b69cfb561e215ac
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-09 08:43:09 +0530

    done testing

--- Recursion/Subsequences.java
+++ Recursion/Subsequences.java
@@ -5,19 +5,20 @@ import java.util.Arrays;
 // Subsequences ==> a contiguous/ non-contiguious sequences, which follows the **order**
 // Striver video => 6 (Recursion series)
 
-public class Subsequences  {
-    public static void print_subseq(int i, int arr[], ArrayList<Integer> arrLi, int n, ArrayList<ArrayList<Integer>> bran) {
+public class Subsequences {
+    public static void print_subseq(int i, int arr[], ArrayList<Integer> arrLi, int n,
+            ArrayList<ArrayList<Integer>> bran) {
         if (i == n) {
             // System.out.println("hi"+i);
             System.out.println(arrLi);
-            bran.add(new ArrayList<>(arrLi));   // should make shallow copy otherwise reference will copy only
+            bran.add(new ArrayList<>(arrLi)); // should make shallow copy otherwise reference will copy only
             return;
         }
         // Important to remeber
-        arrLi.add(arr[i]);  // add like trial room cloth
+        arrLi.add(arr[i]); // add like trial room cloth
         print_subseq(i + 1, arr, arrLi, n, bran);
 
-        arrLi.remove(arrLi.size() - 1);  // then out the cloth 
+        arrLi.remove(arrLi.size() - 1); // then out the cloth
         print_subseq(i + 1, arr, arrLi, n, bran);
     }
 
@@ -26,11 +27,10 @@ public class Subsequences  {
         // System.out.println(Arrays.toString(arr));
         ArrayList<Integer> arrLi = new ArrayList<Integer>();
         ArrayList<ArrayList<Integer>> bran = new ArrayList<ArrayList<Integer>>();
-        
 
         // System.out.println(arrLi);
-        print_subseq(0, arr, arrLi, arr.length,bran);
-        System.out.println("The subsequnces are: "+bran);
+        print_subseq(0, arr, arrLi, arr.length, bran);
+        System.out.println("The subsequnces are: " + bran);
 
     }
 
