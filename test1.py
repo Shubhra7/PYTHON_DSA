b74307b002e97aea0bacdbacc7bb7d70b4ad6a40
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-15 00:59:23 +0530

    saurav fake

--- test1.py
+++ test1.py
@@ -10,30 +10,23 @@ def solve(n,e,d):
             return ((d*e)//n + 1)
     if(n*6)<(e*7):
         return -1
-    
+    print("hi")#
     ans=0
     if((d*e)%n == 0):
         ans = (d*e)//n
     else:
         ans = (d*e)//n + 1
-    left = (ans*n) - (d*e)
-    left_days = d%7
-    req = (left_days*e) - left
-
-    if(req % n == 0):
-        ans += req//n
-    else:
-        ans += ((req//n)+1)
+   
     
     return ans
     
 
-# n=5
-# e=2
-# d=10
+n=5
+e=2
+d=10
 
-n=12
-e=11
-d=7
+# n=12
+# e=11
+# d=7
 
 print(solve(n,e,d))
