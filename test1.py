25760683135ffa925e285ed9eb78d2017c8c5929
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-15 09:58:24 +0530

    2nd highest done

--- test1.py
+++ test1.py
@@ -1,3 +1,17 @@
+import sys
+
+min = min2 = sys.maxsize
+
 arr = [5,4,2,1,2,1,3]
-ans = sorted(set(arr))[1]
-print(ans)
+
+for i in range(len(arr)):
+    if(arr[i]<min):
+        min2=min
+        min=arr[i]
+    elif(arr[i]<min2 and min!=arr[i]):
+        min2 = arr[i]
+
+if(min2 != sys.maxsize):
+    print(min2)
+else:
+    print("Not possible")
