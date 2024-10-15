a825f11130f99c6f7292200425cc62b5edd17e79
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-15 19:38:30 +0530

    done prime indexsum

--- test1.py
+++ test1.py
@@ -1,8 +1,22 @@
-def win_joshep(n,k):
+l=[]
+def isprime(n):
     if(n==1):
-        return 0
-    else:
-        return (win_joshep(n-1,k)+k)%n
+        return False
+    if(n==2 or n==3):
+        return True
+    elif((n**2)-1)%24==0:
+        return True
+    return False
 
-print(win_joshep(41,2)+1)
-print(win_joshep(5,3)+1)
+def prime_index_sum(arr,n):
+    sum=0
+    for i in range(n):
+        if(isprime(i)):
+            sum += arr[i]
+            l.append(arr[i])
+    return sum
+
+arr =[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+n=10
+print(prime_index_sum(arr,n))
+print(l)
