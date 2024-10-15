aaab03e6375f8d1fe9b4d8b92e774dce79e19486
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-15 19:40:47 +0530

    bye

--- test1.py
+++ test1.py
@@ -1,22 +1,11 @@
-l=[]
-def isprime(n):
-    if(n==1):
-        return False
-    if(n==2 or n==3):
-        return True
-    elif((n**2)-1)%24==0:
-        return True
-    return False
+n=int(input("Enter the distance: "))
+area = 3.14*n*n
 
-def prime_index_sum(arr,n):
-    sum=0
-    for i in range(n):
-        if(isprime(i)):
-            sum += arr[i]
-            l.append(arr[i])
-    return sum
+val = area - int(area)
 
-arr =[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-n=10
-print(prime_index_sum(arr,n))
-print(l)
+if(val > 0.5):
+    ans = int(area)+1
+else:
+    ans = int(area)
+
+print("The answer is: ",ans)
