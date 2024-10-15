332fd62218349d44fac37473d1ca07f6b5eb9952
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-15 10:49:59 +0530

    done

--- test1.py
+++ test1.py
@@ -1,3 +1,20 @@
+def min_hour_consume(arr,h):
+    low=1
+    high=max(arr)
+    while(low<=high):
+        mid = low + (high-low)//2
+        req=0
+        for i in range(len(arr)):
+            req += (arr[i]//mid) + (arr[i]%mid != 0)
+        if req<=h:
+            ans=mid
+            high=mid-1
+        else:
+            low=mid+1
+    return ans
+
+
+
 arr = [4,9,11,17]
 
 h=8
