ca378f61002dd34940b5280af2e4bc8bb5543a7d
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-15 23:37:25 +0530

    done

--- test1.py
+++ test1.py
@@ -1,3 +1,17 @@
+def Score(arr):
+    count=0
+    score=0
+    for i in arr:
+        if(i=='H'):
+            score += 2
+            count += 1
+        elif(i=='T'):
+            score -= 1
+            count = 0
+        if(count == 3):
+            return score
+    return score
+
 arr = ['H','T','H','H','T','T','H','T','H','H','T']
 arr1 = ['H','T','H','H','T','T','H','T','H','H','H','T']
 arr2 = ['H','H','H','T','T','T']
