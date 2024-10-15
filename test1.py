ae714c1fa3235408d96e3134458783689efe56b1
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-15 21:35:25 +0530

    gun water done

--- test1.py
+++ test1.py
@@ -1,9 +1,24 @@
-def fibo(n):
-    if(n==0):
-        return 0
-    if(n==1):
-        return 1
-    return fibo(n-1) + fibo(n-2)
+txt="snakewatergunwatergunsnake"
+A=""
+B=""
+word=""
+flag=1
+for i in range(len(txt)):
+    word += txt[i]
+    if(word=='snake' or word=='water' or word=='gun'):
+        if(flag==1):
+            A+=word[0]
+            flag=0
+        else:
+            B+=word[0]
+            flag=1
+        word=""
+count = 0
+for i in range(len(A)):
+    if((A[i]=='s' and B[i]=='w')or(A[i]=='w' and B[i]=='g')or(A[i]=='g' and B[i]=='s')):
+        count += 1
+print(A)
+print(B)
+print("A wining times: ",count)
+    
 
-n=int(input("Enter which fibo number: "))
-print(fibo(n))
