07a3ae220220795e22f5c8cc46dd1f95a12eca15
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-15 21:57:17 +0530

    replace done

--- test1.py
+++ test1.py
@@ -1,24 +1,5 @@
-txt="snakewatergunwatergunsnake"
-A=""
-B=""
-word=""
-flag=1
-for i in range(len(txt)):
-    word += txt[i]
-    if(word=='snake' or word=='water' or word=='gun'):
-        if(flag==1):
-            A+=word[0]
-            flag=0
-        else:
-            B+=word[0]
-            flag=1
-        word=""
-count = 0
-for i in range(len(A)):
-    if((A[i]=='s' and B[i]=='w')or(A[i]=='w' and B[i]=='g')or(A[i]=='g' and B[i]=='s')):
-        count += 1
-print(A)
-print(B)
-print("A wining times: ",count)
-    
-
+n="shavrya"
+ch1='s'
+ch2='a'
+ans = n.replace(ch1,'*').replace(ch2,ch1).replace('*',ch2)
+print(ans)
