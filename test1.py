e46664211d56f2aba572ff8d24e229bbe6b8529d
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-15 11:24:51 +0530

    done

--- test1.py
+++ test1.py
@@ -1,16 +1,18 @@
-dice=4
-num=1234
-arr=list(str(num))
+from collections import Counter
+def delete_earn(nums):
+    d=Counter(nums)
+    mini = min(d)
+    maxi = max(d)
+    pick=mini*d[mini]
+    nopick=0
+    for i  in range(mini+1,maxi+1):
+        newpick = nopick+ i*(d[i])
+        nonewpick = max(pick,nopick)
+        pick = newpick
+        nopick = nonewpick
+        
+    return max(pick,nopick)
 
-sum=0
-if(dice % 2==0):
-    for i in range(1,len(arr),2):
-        sum += int(arr[i])
-else:
-    for i in range(0,len(arr),2):
-        sum += int(arr[i])
 
-print(sum)
-
-
-
+nums = [2,2,3,3,3,4]
+print(delete_earn(nums))
