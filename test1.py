b2568c8cdd0cf94d25d1acfca1554ff1cd9b5995
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-15 16:16:46 +0530

    debug done of my style

---
+++ test1.py
@@ -0,0 +1,31 @@
+def target(n):
+    return (60-(n%60))
+    # return ((n//60 + 1)*60)
+def find_pair(arr):
+    d={}
+    count=0
+    for i in range(len(arr)):
+        print("value: ",arr[i])
+        req = target(arr[i]) 
+        # req = target(arr[i]) - arr[i]
+        if(req in d.keys() and d[req]>0):
+            count += 1
+            d[req] = d[req]-1
+            print(req," ",arr[i])
+        else:
+            d[arr[i]] = d.get(arr[i],0)+1
+        print(d,"  ",count)
+    # print(d)
+    print(count)
+    for i in d:
+        if(d[i]>0):
+            req = target(i)
+            print("Hi: ",req)
+            if(req in d.keys() and d[req]>0):
+                count += 1
+                d[req]=d[req]-1
+    print(count)
+
+
+arr=[0,2,58,58,2,60]
+print(find_pair(arr))
