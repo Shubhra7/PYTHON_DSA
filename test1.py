ee61e76f7d0e10001baf70ddc2cf9b2c1466b280
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-15 15:29:32 +0530

    stock basic done

--- test1.py
+++ test1.py
@@ -1,1 +1,13 @@
-13 sep 
+class Solution:
+    def maxProfit(self, prices) -> int:
+        ans=0
+        mini=prices[0]
+        for i in range(1,len(prices)):
+            if(ans < (prices[i] - mini)):
+                ans = prices[i]-mini
+            if(mini > prices[i]):
+                mini = prices[i]
+        return ans 
+        
+obj = Solution()
+print(obj.maxProfit([7,1,5,3,6,4]))
