4d8c31b202faf6e8de95e6ee03e3e36a1f6bbeec
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-15 23:27:15 +0530

    BG done

--- test1.py
+++ test1.py
@@ -1,3 +1,15 @@
 txt = "BGGBGBBG"
 arr = list(txt)
 t=2
+for i in range(t):
+    j=0
+    while (j<len(arr)-1):
+        if(arr[j]=='B' and arr[j+1]=='G'):
+            temp = arr[j]
+            arr[j] = arr[j+1]
+            arr[j+1] = temp
+            j += 2
+        else:
+            j+=1
+
+print("".join(arr))
