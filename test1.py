dad059181b0f4b6d11875865a7207d92aa9992d8
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-15 10:17:00 +0530

    done

--- test1.py
+++ test1.py
@@ -1,3 +1,21 @@
 arr=[1,2,3,4,5,6,7]
 k=3
 arr = arr[::-1]
+i=0
+j=k-1
+while(i<j):
+    temp = arr[i]
+    arr[i]=arr[j]
+    arr[j]=temp
+    i += 1
+    j -= 1
+print(arr)
+i=k
+j=len(arr)-1
+while(i<j):
+    temp = arr[i]
+    arr[i]=arr[j]
+    arr[j]=temp
+    i += 1
+    j -= 1
+print(arr)
