d835de0bdd67ae7857713bbf03f2b22d894dad9f
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-15 09:55:39 +0530

    short cut done

--- test1.py
+++ test1.py
@@ -1,27 +1,3 @@
-arr=[1,6,0,-1,-2,3,4,8,6,4,3,2,1,0]
-
-count = 0
-p=[]
-maxi=0
-j=0
-for i in range(1,len(arr)):
-    if(arr[i]<arr[i-1]):
-        if arr[i-1] not in p:
-            print(arr[i-1],end=" ")
-            p.append(arr[i-1])
-        p.append(arr[i])
-        print(arr[i],end=" ")
-        count += 1
-    else:
-        print()
-        maxi=max(maxi,count+1)
-        count = 0
-        j=i
-if(maxi < count+1):
-    maxi = count+1
-    j=i
-# maxi = max(maxi,count+1)
-print()
-print(maxi)
-print(p)
-print(p[-maxi:len(p)])
+arr = [5,4,2,1,2,1,3]
+ans = sorted(set(arr))[1]
+print(ans)
