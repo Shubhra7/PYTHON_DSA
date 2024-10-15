c6d0d145973b2aebb7b057164633954fb3fcadad
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-15 11:29:30 +0530

    13 sep should start

--- test1.py
+++ test1.py
@@ -1,18 +1,1 @@
-from collections import Counter
-def delete_earn(nums):
-    d=Counter(nums)
-    mini = min(d)
-    maxi = max(d)
-    pick=mini*d[mini]
-    nopick=0
-    for i  in range(mini+1,maxi+1):
-        newpick = nopick+ i*(d[i])
-        nonewpick = max(pick,nopick)
-        pick = newpick
-        nopick = nonewpick
-        
-    return max(pick,nopick)
-
-
-nums = [2,2,3,3,3,4]
-print(delete_earn(nums))
+13 sep 
