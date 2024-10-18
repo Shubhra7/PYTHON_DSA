10ed203a468103dee072d251f407faa12b5441db
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-18 20:31:35 +0530

    start sub seq

--- test1.py
+++ test1.py
@@ -1,16 +1,4 @@
-def paid_candy(n,arr,m):
-    tot = 0
-    arr.sort()
-    for i in arr:
-        if(i%5 == 0):
-            tot += 1
-        else:
-            if(i<m):
-                m -= i
-                tot += 1
-    return tot
-
-
-
-print(paid_candy(4,[5,4,15,6],5))
-print(paid_candy(4,[5,8,15,6],5))
+arr = [3,2,1]
+ds=[]
+ans=[]
+find_seq(0,arr,ds,ans)
