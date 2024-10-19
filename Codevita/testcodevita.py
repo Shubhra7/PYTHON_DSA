81a8a1a2a8597b3c2a05b733b65f57da461e93a3
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-19 09:28:11 +0530

    let do

--- Codevita/testcodevita.py
+++ Codevita/testcodevita.py
@@ -10,60 +10,65 @@
 # y="kkkkkk"
 # S,R = 4,2
 
-
-stra = 0
-revi =0
-i=0
-j=1
-flag1,flag2 =0,0
-ds,dr =0,0
-
-
-while (j<len(x)):
+def XFromY(x,y,S,R):
+    stra = 0
+    revi =0
+    i=0
+    j=1
     flag1,flag2 =0,0
     ds,dr =0,0
-    val = x[i:j]
-    if( val in y):
-        if((j+1) in range(len(x)+1) and x[i:j+1] in y):
-            flag1=1
-        ds = j - i
-    if(val[::-1] in y):
-        if((j+1) in range(len(x)+1) and x[i:j+1][::-1] in y):
-            flag2=1
-        dr = j - i
-    if(flag1==1 or flag2==1):
-        j += 1
-        continue
-    if(ds == dr and ds!=0 and dr!=0):
-        if(S > R):
+    while (j<len(x)):
+        flag1,flag2 =0,0
+        ds,dr =0,0
+        val = x[i:j]
+        if( val in y):
+            if((j+1) in range(len(x)+1) and x[i:j+1] in y):
+                flag1=1
+            ds = j - i
+        if(val[::-1] in y):
+            if((j+1) in range(len(x)+1) and x[i:j+1][::-1] in y):
+                flag2=1
+            dr = j - i
+        if(flag1==1 or flag2==1):
+            j += 1
+            continue
+        if(ds == dr and ds!=0 and dr!=0):
+            if(S > R):
+                revi += 1
+                i=j
+                j+=1
+            else:
+                stra += 1
+                i=j
+                j+=1
+        elif(ds > dr):
+            stra += 1
+            i=j
+            j+=1
+        elif(ds < dr):
             revi += 1
             i=j
             j+=1
         else:
-            stra += 1
-            i=j
-            j+=1
-    elif(ds > dr):
+            j += 1
+
+    if(flag1 == 1):
         stra += 1
         i=j
         j+=1
-    elif(ds < dr):
+    elif(flag2 == 1):
         revi += 1
         i=j
         j+=1
+    if(stra == 0 and revi == 0):
+        print("Impossible")
     else:
-        j += 1
+        ans = (stra*S) + (revi*R)
+        print(ans)
 
-if(flag1 == 1):
-    stra += 1
-    i=j
-    j+=1
-elif(flag2 == 1):
-    revi += 1
-    i=j
-    j+=1
-if(stra == 0 and revi == 0):
-    print("Impossible")
-else:
-    ans = (stra*S) + (revi*R)
-    print(ans)
+
+
+X = input()
+Y = input()
+S,R = map(int,input().split())
+XFromY(X,Y,S,R)
