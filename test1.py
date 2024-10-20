3071c9dc2a5191f8308e4fce1e37da3b6668ab98
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-20 10:05:09 +0530

    okay

--- test1.py
+++ test1.py
@@ -1,26 +1,28 @@
-def fact(n):
+def solve(n,e,d):
     if(n==0):
-        return 1
-    else:
-        return n*fact(n-1)
+        return -1
+    if(e==0 or d==0):
+        return 0
+    if((e*7) > (n*6)):
+        return -1
+    if(d <7):
+        val = d*e
+        return (val//n +(val%n !=0))
+    
+    week = d//7 
+    need = week*7*e
+    pur_count = (need//n) + (need%n != 0)
+    left = (pur_count*n) - need
+    left_days = d%7
+    req = (left_days*e) - left
 
-def permut(s):
-    d={}
-    vow = ['a','e','i','o','u']
-    count = 0
-    for i in range(len(s)):
-        if s[i] not in vow:
-            count += 1
-            d[s[i]] = d.get(s[i],0)+1
-    dual=1
-    for i in d.values():
-        if i > 1:
-            dual *= fact(i)
-    print(count)
-    return int(fact(count)/dual)
+    pur_count += (req//n) + (req%n != 0)
 
-
-s="ybghjahebuyitob"
-print(permut(s.lower()))
+    return pur_count
 
 
+
+n=5
+e=2
+d=10
+print(solve(n,e,d))
