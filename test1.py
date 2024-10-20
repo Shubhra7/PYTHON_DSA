333ee1c3b39dac12d13e040879888273e1eab0a8
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-20 14:33:57 +0530

    done nilangan

--- test1.py
+++ test1.py
@@ -1,29 +1,3 @@
-def A_Wins(s):
-    word=""
-    valA = []
-    valB =[]
-    flag=0
-    for i in s:
-        word += i
-        if(word=="snake" or word=="water" or word=="gun"):
-            if(flag==0):
-                valA.append(word[0])
-                word=""
-                flag=1
-            else:
-                valB.append(word[0])
-                word=""
-                flag=0
-    A=0
-    for i in range(len(valA)):
-        if((valA[i]=='s' and valB[i]=='w') or
-           (valA[i]=='w' and valB[i]=='g')or
-           (valA[i]=='g' and valB[i]=='s')):
-            A+=1
-    return A
-
-s="snakewatergunsnake"
-print("A wins: ",A_Wins(s)," times.")
-print()
-s1="snakesnakewatergun"
-print("A wins: ",A_Wins(s1)," times.")
+H,v,vn = 10,20,5
+ans = H * (v/vn)**2
+print(int(ans))
