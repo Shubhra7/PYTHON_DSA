c57a1411415ccfb9de75be21de3727002261fce9
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-20 16:17:23 +0530

    File max version done

--- test1.py
+++ test1.py
@@ -1,17 +1,14 @@
-def score(arr):
-    hcount=0
-    sc=0
-    for i in arr:
-        if(i=='H'):
-            sc += 2
-            hcount+=1
-        else:
-            sc -= 1
-            hcount=0
-        if(hcount==3):
-            return sc
-    return sc
+def max_version(arr1,n):
+    maxi=-1
+    for i in arr1:
+        l=i.split('_')
+        if(l[0]=='File' and l[1].isdigit()):
+            if(int(l[1]) > maxi):
+                maxi=int(l[1])
+    return maxi
 
 
-arr1 = ['T','T','T']
-print(score(arr1))
+
+n=4
+arr = ["File_2","File_1","File_6s","Fil_7"]
+print(max_version(arr,n))
