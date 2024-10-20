aae14e202be1d5fe805ff7d19f0917554c7e03b2
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-20 12:12:29 +0530

    done

--- test1.py
+++ test1.py
@@ -1,5 +1,19 @@
-import bisect
+def solve(arr,h):
+    low=1
+    high=max(arr)
+    while(low<=high):
+        mid = low +(high -low)//2
+        req=0
+        for i in arr:
+            req += (i//mid) + (i%mid!=0)
+        if(req <=h):
+            ans=mid
+            high=mid-1
+        else:
+            low = mid+1
+    return ans
 
-arr=[1,2,3,4,5]
+arr=[4,9,11,17]
+h=8
 
-print(bisect.bisect_right(arr,3))
+print(solve(arr,h))
