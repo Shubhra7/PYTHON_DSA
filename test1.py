691cf78c15f41414bf64bde51a02bd94824c340c
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-20 09:24:26 +0530

    own testing checked

--- test1.py
+++ test1.py
@@ -1,9 +1,1 @@
-def sq_fibo(n):
-    if(n==0 or n==1):
-        return 1
-    else:
-        return (sq_fibo(n-1)**2) + (sq_fibo(n-2)**2)
-    
-
-n=int(input())
-print(sq_fibo(n)%47)
+print('a'.isdigit())
