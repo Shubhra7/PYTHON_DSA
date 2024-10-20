48877f6c86be42e3b4d42a412d2a161632162faa
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-20 11:06:24 +0530

    rotate array done

--- test1.py
+++ test1.py
@@ -1,1 +1,22 @@
-print(bin(0x2d))
+arr=[1,2,3,4,5,6,7]
+k=1
+arr = arr[::-1]
+
+i=0
+j=k-1
+while( i<j):
+    arr[j] = arr[i] ^ arr[j]
+    arr[i] = arr[i] ^ arr[j]
+    arr[j] = arr[i] ^ arr[j]
+    i+=1
+    j -=1
+
+i=k
+j=len(arr)-1
+while( i<j):
+    arr[j] = arr[i] ^ arr[j]
+    arr[i] = arr[i] ^ arr[j]
+    arr[j] = arr[i] ^ arr[j]
+    i+=1
+    j -=1
+print(arr)
