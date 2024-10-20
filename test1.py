6211dbf826681829b685dcc072125eda75ca9a97
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-20 14:30:39 +0530

    game A win

--- test1.py
+++ test1.py
@@ -1,10 +1,29 @@
-def fibo(n):
-    if(n==0):
-        return 0
-    if(n==1):
-        return 1
-    else:
-        return fibo(n-1) + fibo(n-2)
+def A_Wins(s):
+    word=""
+    valA = []
+    valB =[]
+    flag=0
+    for i in s:
+        word += i
+        if(word=="snake" or word=="water" or word=="gun"):
+            if(flag==0):
+                valA.append(word[0])
+                word=""
+                flag=1
+            else:
+                valB.append(word[0])
+                word=""
+                flag=0
+    A=0
+    for i in range(len(valA)):
+        if((valA[i]=='s' and valB[i]=='w') or
+           (valA[i]=='w' and valB[i]=='g')or
+           (valA[i]=='g' and valB[i]=='s')):
+            A+=1
+    return A
 
-n=int(input("Enter the number: "))
-print(fibo(n))
+s="snakewatergunsnake"
+print("A wins: ",A_Wins(s)," times.")
+print()
+s1="snakesnakewatergun"
+print("A wins: ",A_Wins(s1)," times.")
