09c91eac70bd6f6cc8e3b55018b852c6d24165d6
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-20 09:26:20 +0530

    xor sum done

--- test1.py
+++ test1.py
@@ -1,1 +1,11 @@
-print('a'.isdigit())
+def cal(arr):
+    ans1, ans2 = arr[0],arr[1]
+    for i in range(2,len(arr)):
+        if(i%2==0):
+            ans1 += arr[i]
+        else:
+            ans2 ^= arr[i]
+    return ans1 + ans2
+
+arr = [1,2,4,2,1]
+print(cal(arr))
