511c31cdff311dd2ecd45fca486a013660e9840f
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-20 13:44:12 +0530

    tree shadow done

--- test1.py
+++ test1.py
@@ -1,17 +1,10 @@
-def isprime(n):
-    if(n==2 or n==3):
-        return True
-    if(n>3):
-        if((n**2)-1)%24==0:
-            return True
-    return False
-def sum_prime_index(arr):
-    ans=0
-    for i in range(len(arr)):
-        if(isprime(i)):
-            ans += arr[i]
-    return ans
+n=int(input("Enter the distance:" ))
+area=3.14*n*n
 
+val = area - int(area)
 
-arr =[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-print(sum_prime_index(arr))
+if(val > 0.5):
+    area = int(area)+1
+else:
+    area = int(area)
+print("The answer is: ",area)
