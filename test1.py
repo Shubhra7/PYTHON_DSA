13bea268b957cdcef9a03b8f6c54cb36509cd2ec
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-20 15:08:09 +0530

    12 hour done

--- test1.py
+++ test1.py
@@ -1,7 +1,18 @@
-def second_lar_count(arr):
-    dem = sorted(set(arr),reverse=True)
-    return arr.count(dem[1])
+def convert_to_12Hour(hour,min):
+    if(hour >= 12):
+        period="PM"
+        if(hour > 12):
+            hour = hour-12
+    else:
+        if(hour == 0):
+            ansH=12
+        period="AM"
+    print("The time is: ",hour,":",min,period)
 
+hour =  int(input("Enter the hour(0-23): "))
+min = int(input("Enter the minute(0-59): "))
 
-arr = [1,2,3,3,4,4]
-print(second_lar_count(arr))
+if(hour < 0 or hour > 23 or min<0 or min > 59):
+    print("Invalid time input")
+else:
+    convert_to_12Hour(hour,min)
