c339af8740f1a8e9de87a2164fae050f10e3f0a5
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-20 14:52:52 +0530

    done

--- test1.py
+++ test1.py
@@ -1,25 +1,7 @@
-s1="adventure"
-s2="advenfuture"
+def second_lar_count(arr):
+    dem = sorted(set(arr),reverse=True)
+    return arr.count(dem[1])
 
-row = len(s2)
-col = len(s1)
 
-dp = [[0 for i in range(col+1)]for j in range(row+1)]
-maxi=0
-ro,co=-1,-1
-
-for i in range(1,row+1):
-    for j in range(1,col+1):
-        if(s1[j-1] == s2[i-1]):
-            dp[i][j] = dp[i-1][j-1]+1
-            if(maxi < dp[i][j]):
-                maxi = dp[i][j]
-                ro,co = i,j
-        else:
-            dp[i][j]=0
-
-print(maxi)
-ans =""
-for i in range(ro-maxi,ro):
-    ans += s2[i]
-print(ans)
+arr = [1,2,3,3,4,4]
+print(second_lar_count(arr))
