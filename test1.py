757f68fbc8d8980876823fb0190147e9264f14cd
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-20 16:20:12 +0530

    done

--- test1.py
+++ test1.py
@@ -1,11 +1,1 @@
-def max_profit(price):
-    pick=-price[0]
-    nopick=0
-    for i in range(1,len(price)):
-        pick = max(pick, nopick-price[i])
-        nopick = max(nopick, pick+price[i])
-        print(pick," ",nopick)
-    return nopick
-
-price = [7,1,5,3,6,4]
-print("The maximum profit will be: ",max_profit(price))
+print("Accenture prev coding done")
