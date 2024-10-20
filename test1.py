ebab2d487e428f758a8813245ae065a122366234
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-20 13:07:27 +0530

    Stone color start

--- test1.py
+++ test1.py
@@ -1,22 +1,2 @@
-
-def tar_mes(n):
-    ans = (n//60 + 1)*60
-    return ans
-def combo(arr):
-    d={}
-    p=[]
-    count=0
-    for i in arr:
-        tar = tar_mes(i)-i
-        if(d.get(tar,0) > 0):
-            d[tar] -= 1
-            count += 1
-            p.append([i,tar])
-        else:
-            d[i]=d.get(i,0)+1
-    print(p)
-    print(count)
-    return count
-
-arr = [2,58,58,2,60,60]
-print(combo(arr))
+Stone = "RRRBGBRBBB"
+INS= "BBBR"
