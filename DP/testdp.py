499fbfb09ccc8c377a1f69cde54c86f880bd8733
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-23 20:14:25 +0530

    okay

--- DP/testdp.py
+++ DP/testdp.py
@@ -1,22 +1,18 @@
-import copy
-def seq(ind,arr,ds,ans,s,target):
-    if(ind == len(arr)):
-        if(target == s):
-            ans.append(copy.deepcopy(ds))
-            return 1
-        return 0
-    ds.append(arr[ind])
-    s += arr[ind]
-    r= seq(ind+1,arr,ds,ans,s,target)
-    ds.pop()
-    s -= arr[ind]
-    l=seq(ind+1,arr,ds,ans,s,target)
-    return r+l
+import sys
 
-arr = [3,2,1]
-ds=[]
-ans=[]
-target=3
-s=0
-print(seq(0,arr,ds,ans,s,target))
-print(ans)
+def frogJump(n, heights) -> int:
+
+    # Write your code here.
+    prev, prev2=0,0
+    for i in range(1,len(arr)):
+        ls = prev + abs(heights[i] - heights[i-1])
+        rs=sys.maxsize
+        if i>1:
+            rs = prev2 + abs(heights[i] - heights[i-2])
+        prev2 = prev
+        prev = min(ls,rs)
+    return prev
+
+# arr=[10,20,30,10]
+arr=[10,50,10]
+print(frogJump(len(arr),arr))
