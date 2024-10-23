55a53c3049b958dbce2d4f53399dae7dddc2622f
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-23 20:20:35 +0530

    space optimization done lets do recursion

--- DP/Frog_Jump.py
+++ DP/Frog_Jump.py
@@ -7,16 +7,33 @@
 # ---------------------
 
 import sys
-# arr=[7,4,4,2,6,6,3,4]
-arr=[10,50,10]
+arr=[7,4,4,2,6,6,3,4]
+# arr=[10,50,10]
 dp=[0 for i in range(len(arr))]
-dp[1]=abs(arr[1]-arr[0])
+# dp[1]=abs(arr[1]-arr[0])
 for i in range(1,len(arr)):
-    leftval = dp[i-1] + abs(dp[i]-dp[i-1])
+    leftval = dp[i-1] + abs(arr[i]-arr[i-1])
     rightval = sys.maxsize
     if(i>1):
-        rightval = dp[i-2] + abs(dp[i]-dp[i-2])
+        rightval = dp[i-2] + abs(arr[i]-arr[i-2])
     dp[i] = min(leftval,rightval)
 
 print(dp)
 print(dp[-1])
+
+
+# ---------------------
+# Space optimization Method
+# ---------------------
+
+# arr = [10,50,10]
+arr = [7,4,4,2,6,6,3,4]
+prev, prev2 = 0,0
+for i in range(1,len(arr)):
+    ls = prev + abs(arr[i] - arr[i-1])
+    rs = sys.maxsize
+    if i>1:
+        rs = prev2 + abs(arr[i]-arr[i-2])
+    prev2 = prev
+    prev = min(ls,rs)
+print(prev)
