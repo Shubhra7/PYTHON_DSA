da3561fa9dd609f1a9fc01b10a721624b4bea6ef
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-25 16:00:22 +0530

    done

--- test1.py
+++ test1.py
@@ -1,10 +1,13 @@
-text = "hello.hubhrajit.name.is.shubhrajit"
+def special_fibo(n):
+    if(n==0 or n==1):
+        return 1
+    a=1
+    b=1
+    for i in range(2,n+1):
+        c = (a**2)+(b**2)
+        a=b
+        b=c
+    return (b%47)
 
-l=list(text.split('.'))
-print(l)
-
-maxi=""
-for i in l:
-    if(len(i) > len(maxi)):
-        maxi=i
-print(maxi)
+n=int(input("Enter the value of n: "))
+print(special_fibo(n))   
