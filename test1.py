0451714ad691993c215f8f1a90fc857d7b0d6801
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-25 11:41:11 +0530

    snake water gun done

--- test1.py
+++ test1.py
@@ -1,4 +1,27 @@
-s1='shavrya'
+def A_Wins(s):
+    i=0
+    j=1
+    word=""
+    A,B =[],[]
+    for i in range(len(s)):
+        word += s[i]
+        if(word == 'snake' or word=='water' or word=='gun'):
+            if(j==1):
+                A.append(word[0])
+                j *= -1
+            elif(j==-1):
+                B.append(word[0])
+                j *= -1
+            word=""
+    Ac=0
+    for i in range(len(A)):
+        if(A[i]=='s' and B[i]=='w') or (A[i]=='w' and B[i]=='g') or (A[i]=='g' and B[i]=='s'):
+            Ac += 1
+    return Ac
+ 
 
-ans = s1.replace('a','*').replace('s','a').replace('*','s')
-print(ans)
+s="snakewatergunsnake"
+print("A wins: ",A_Wins(s)," times.")
+print()
+s1="snakesnakewatergun"
+print("A wins: ",A_Wins(s1)," times.")
