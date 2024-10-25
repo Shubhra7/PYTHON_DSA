2991e7385993400883f6fc0c9977e97a7e0a4d95
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-25 10:36:38 +0530

    candy done

--- test1.py
+++ test1.py
@@ -1,13 +1,17 @@
-def divident_pos(arr,divi,quo,rem):
-    val = (divi * quo)+rem
-    if(val in arr):
-        return arr.index(val)
-    else:
-        return -1
+def paid_candy(n,arr,m):
+    count=0
+    for i in range(n):
+        if(arr[i]%5 == 0):
+            count += 1
+        elif(m >= arr[i]):
+            m -= arr[i]
+            count += 1
+    return count
 
-arr = [5,6,7,9,10]
-divisior = 2
-quotient = 3
-rem = 1
+n=3
+arr=[5,15,105]
+m=8
 
-print(divident_pos(arr, divisior, quotient, rem))
+print(paid_candy(n,arr,m))
+print(paid_candy(4,[5,4,15,6],5))
+print(paid_candy(4,[5,8,15,6],5))
