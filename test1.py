c105e44b73bbc66cfd011ed621063eef057cd30a
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-25 15:55:48 +0530

    done

--- test1.py
+++ test1.py
@@ -1,27 +1,10 @@
-def solve(n,e,d):
-    if(n==0):
-        return -1
-    if(e==0 or d==0):
-        return 0
-    if(d<7):
-        if((d*e)%n == 0):
-            return (d*e)//n
-        else:
-            return ((d*e)//n + 1)
-    if((n*6) < (e*7)):
-        return -1
-    
-    weeks = (d//7)
-    ans=0
-    ans += ((weeks*e*7)//n) + ((weeks*e*7)%n!=0)
-    left = (ans*n) - (weeks*e*7)
-    left_days = d%7
-    req = (left_days*e) - left
-    ans += (req//n) + (req%n!=0)
-    return ans
+text = "hello.hubhrajit.name.is.shubhrajit"
 
-n=5
-e=2
-d=10
+l=list(text.split('.'))
+print(l)
 
-print(solve(n,e,d))
+maxi=""
+for i in l:
+    if(len(i) > len(maxi)):
+        maxi=i
+print(maxi)
