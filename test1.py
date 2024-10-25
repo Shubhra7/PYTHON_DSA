6cffac984a8ac40388066641cde9e4549444f5ec
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-25 10:22:21 +0530

    max version file done

--- test1.py
+++ test1.py
@@ -1,14 +1,15 @@
-s2="bbkallalu"
-s1="abbkalllup"
+def max_version(arr,n):
+    maxi=-1
+    for i in range(n):
+        l=list(arr[i].split('_'))
+        if (l[0]=='File' and l[1].isdigit()):
+            if(maxi < int(l[1])):
+                maxi = int(l[1])
+    return maxi
 
-l1 = list(s1)
-l1.sort()
-
-l2 = list(s2)
-l2.sort()
-
-if("".join(l1) == "".join(l2)):
-    print("Yes")
-else:
-    print("No")
+n=4
+arr1 = ["File_2","File_1","File_6","Fil_7"]
+print(max_version(arr1,n))
+arr = ["File_2","File_1","File_6s","Fil_7"]
+print(max_version(arr,n))
 
