296f8ac7ddbc909d0efffdfa6f5086be750ec6e5
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-25 11:33:17 +0530

    replace done

--- test1.py
+++ test1.py
@@ -1,25 +1,4 @@
-s1 = "adventure"
-s2 = "fadvenuture"
+s1='shavrya'
 
-row = len(s2)
-col = len(s1)
-
-dp =[[0 for i in range(col+1)]for j in range(row+1)]
-maxi=-1
-ro,co=-1,-1
-
-for i in range(1,row+1):
-    for j in range(1,col+1):
-        if(s2[i-1] == s1[j-1]):
-            dp[i][j] = dp[i-1][j-1]+1
-            if(maxi < dp[i][j]):
-                maxi = dp[i][j]
-                ro,co = i,j
-        else:
-            dp[i][j]=0
-
-print("Max same length: ",maxi)
-ans =""
-for i in range(ro-1,ro-maxi-1,-1):
-    ans = s2[i]+ans
+ans = s1.replace('a','*').replace('s','a').replace('*','s')
 print(ans)
