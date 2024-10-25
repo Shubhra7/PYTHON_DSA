e3dc6520eb45ca896b6935496012474a74ffc2cf
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-25 12:45:25 +0530

    stone color done

--- test1.py
+++ test1.py
@@ -1,9 +1,8 @@
-def win_joshep(n,k):
-    if(n==1):
-        return 0
-    else:
-        return (win_joshep(n-1,k)+k)%n
+Stone = "RRRBGBRBBB"
+INS= "BBBR"
 
-print(win_joshep(41,2)+1)
-print(win_joshep(5,3)+1)
-print(win_joshep(1,3)+1)
+id=0
+for i in range(len(INS)):
+    if(Stone[id] == INS[i]):
+        id += 1
+print(id+1)
