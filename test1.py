9a88f3a5e4293398e22b3eb7bf145d2dbe73c666
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-25 13:04:48 +0530

    delete and earn

--- test1.py
+++ test1.py
@@ -1,8 +1,17 @@
-Stone = "RRRBGBRBBB"
-INS= "BBBR"
+from collections import Counter
 
-id=0
-for i in range(len(INS)):
-    if(Stone[id] == INS[i]):
-        id += 1
-print(id+1)
+def delete_earn(nums):
+    d=Counter(nums)
+    mini=min(nums)
+    maxi = max(nums)
+    pick = mini*d[mini]
+    nopick = 0
+    for i in range(mini+1, maxi+1):
+        newpick = nopick + (i*d[i])
+        newnopick = max(nopick,pick)
+        pick, nopick = newpick, newnopick
+        print(pick," ",nopick)
+    return max(pick,nopick)
+
+nums = [2,2,3,3,3,4]
+print(delete_earn(nums))
