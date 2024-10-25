da4269f9432a60f5c6c547a8981dc03021f6055c
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-25 15:49:01 +0530

    candy done

--- test1.py
+++ test1.py
@@ -1,26 +1,27 @@
-arr=[9,8,7,5,4,3,2,1,0,-1,-2,3,4,8,6,4,3,2,1,0]
-# arr=[1,6,0,-1,-2,3,4,8,6,4,3,2,1,0]
-# arr=[1]
-count =0
-i=1
-maxi=0
-while i< len(arr):
-    if(arr[i] < arr[i-1]):
-        count += 1
-    else:
-        if count > maxi:
-            maxi = count
-            ro=i
-        count = 0
-    i += 1
-
-if(count > maxi):
-    maxi=count
-    ro=len(arr)
-
-print("val---")
-print(maxi)
-print(ro)
-print(ro-maxi-1," ",ro)
-print(arr[ro-maxi-1:ro])
+def solve(n,e,d):
+    if(n==0):
+        return -1
+    if(e==0 or d==0):
+        return 0
+    if(d<7):
+        if((d*e)%n == 0):
+            return (d*e)//n
+        else:
+            return ((d*e)//n + 1)
+    if((n*6) < (e*7)):
+        return -1
     
+    weeks = (d//7)
+    ans=0
+    ans += ((weeks*e*7)//n) + ((weeks*e*7)%n!=0)
+    left = (ans*n) - (weeks*e*7)
+    left_days = d%7
+    req = (left_days*e) - left
+    ans += (req//n) + (req%n!=0)
+    return ans
+
+n=5
+e=2
+d=10
+
+print(solve(n,e,d))
