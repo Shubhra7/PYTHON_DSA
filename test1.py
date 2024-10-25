f07a83ce4c3067c9dfdbf44349dd5c53512b927b
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-25 11:01:14 +0530

    done

--- test1.py
+++ test1.py
@@ -1,26 +1,16 @@
-def max_matc(m,n):
-    count = 0
-    bound = min(len(m),len(n))
-    for i in range(1,bound+1):
-        if(m[-i] == n[-i]):
-            count += 1
-        else:
-            return count
-    return count
+def convert_to_12Hour(hour,min):
+    if(hour >= 12):
+        period='PM'
+        if(hour>12):
+            hour = hour - 12
+    else:
+        period = 'AM'
+    print("The time is:",hour,":",min,period)
 
-def max_rhyme(arr,match):
-    mini = 0
-    ans=""
-    for i in arr:
-        val = max_matc(i,match)
-        print(val,": ",i)
-        if(val > mini or (val == mini and len(i)<len(ans))):
-            mini=val
-            ans = i
-    return ans
-        
+hour =  int(input("Enter the hour(0-23): "))
+min = int(input("Enter the minute(0-59): "))
 
-arr = ["gender","blender","blunder","under"]
-match = "thunder"
-
-print(max_rhyme(arr,match))
+if(hour < 0 or hour > 23 or min<0 or min > 59):
+    print("Invalid time input")
+else:
+    convert_to_12Hour(hour,min)
