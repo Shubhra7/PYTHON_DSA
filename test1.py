fb6bff5eeab76e9da5d4c446b9233312ff04ad7a
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-25 10:57:38 +0530

    done max match

--- test1.py
+++ test1.py
@@ -1,16 +1,26 @@
-t=2
-text1="bggbgbbg"
-text = list(text1)
-
-for _ in range(t):
-    # for i in range(0,len(text)-1,):
-    i=0
-    while(i<len(text)-1):
-        if(text[i]=='b' and text[i+1]=='g'):
-            text[i], text[i+1] = text[i+1],text[i]
-            i+=2
+def max_matc(m,n):
+    count = 0
+    bound = min(len(m),len(n))
+    for i in range(1,bound+1):
+        if(m[-i] == n[-i]):
+            count += 1
         else:
-            i+=1
-    print("".join(text))
+            return count
+    return count
 
-print("".join(text))
+def max_rhyme(arr,match):
+    mini = 0
+    ans=""
+    for i in arr:
+        val = max_matc(i,match)
+        print(val,": ",i)
+        if(val > mini or (val == mini and len(i)<len(ans))):
+            mini=val
+            ans = i
+    return ans
+        
+
+arr = ["gender","blender","blunder","under"]
+match = "thunder"
+
+print(max_rhyme(arr,match))
