9d75eb7e87a57d83a721f06bc287d36cfcfd600e
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-25 14:20:56 +0530

    rotate done

--- test1.py
+++ test1.py
@@ -1,13 +1,21 @@
-text = "TeamATeamBTeamATeamBTeamATeamBTeamATeamA"
+arr=[1,2,3,4,5,6,7]
+k=3
+arr=arr[::-1]
 
-Aval, Bval=0,0
-i=4
-for j in range(4,len(text),5):
-    if(text[j]=='A'):
-        Aval += 1
-    if(text[j]=='B'):
-        Bval += 1
-if(Aval > Bval):
-    print("TeamA")
-else:
-    print("TeamB")
+i,j = 0,k-1
+while i<j:
+    temp=arr[i]
+    arr[i]=arr[j]
+    arr[j]=temp
+    i+=1
+    j-=1
+
+i,j = k,len(arr)-1
+while i<j:
+    temp = arr[i]
+    arr[i]=arr[j]
+    arr[j]=temp
+    i+=1
+    j-=1
+
+print(arr)
