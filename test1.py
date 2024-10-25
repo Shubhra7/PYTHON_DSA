3e065b0c99a12c51a10ee8c86fac7edf5c59633e
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-25 12:10:29 +0530

    done

--- test1.py
+++ test1.py
@@ -1,24 +1,21 @@
-def isgoogly(n):
+def prime_index_sum(arr,n):
     sum=0
-    while n>0:
-        rem = n %10
-        sum = sum +rem
-        n =n //10
-    if(isprime(sum)):
-        return True
-    else:
-        return False
+    for i in range(len(arr)):
+        if(isprime(i)):
+            print(arr[i])
+            sum += arr[i]
+    return sum
 
 def isprime(n):
-    if(n<3 and n>0):
+    if(n>1 and n<=3):
         return True
     elif(n>3):
-        if(((n**2)-1) % 24 ==0):
+        if(((n**2)-1)%24==0):
             return True
     return False
 
-n=141
-if(isgoogly(n)):
-    print("Yes")
-else:
-    print("No")
+
+arr =[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+n=10
+print(prime_index_sum(arr,n))
+# print(l)
