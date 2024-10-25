3602631518cec4831c106d6708dd575b4113f27b
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-25 12:04:28 +0530

    Done googly

--- test1.py
+++ test1.py
@@ -1,11 +1,24 @@
-def covert(n):
-    l=list(n.split('0'))
-    ans=""
-    for i in l:
-        val = len(i)
-        ans += chr(ord('A')-1+val)
-    return ans
+def isgoogly(n):
+    sum=0
+    while n>0:
+        rem = n %10
+        sum = sum +rem
+        n =n //10
+    if(isprime(sum)):
+        return True
+    else:
+        return False
 
+def isprime(n):
+    if(n<3 and n>0):
+        return True
+    elif(n>3):
+        if(((n**2)-1) % 24 ==0):
+            return True
+    return False
 
-n=input("Enter the string: ")
-print(covert(n))
+n=141
+if(isgoogly(n)):
+    print("Yes")
+else:
+    print("No")
