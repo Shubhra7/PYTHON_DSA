c18290da10e112953a0bef2c03927e7ae6dac99f
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-25 10:33:16 +0530

    dividend done

--- test1.py
+++ test1.py
@@ -1,19 +1,13 @@
-input1 = "abc bcd cda"
-input2 = "abc    bad"
+def divident_pos(arr,divi,quo,rem):
+    val = (divi * quo)+rem
+    if(val in arr):
+        return arr.index(val)
+    else:
+        return -1
 
+arr = [5,6,7,9,10]
+divisior = 2
+quotient = 3
+rem = 1
 
-in1, in2 = 0,0
-
-for i in range(len(input1)):
-    if(input1[i] == ' '):
-        in1 += 1
-
-for i in range(len(input2)):
-    if(input2[i] == ' '):
-        in2 += 1
-
-val = abs(in1 - in2)
-if(val%2 == 0):
-    print("Even: ",val)
-else:
-    print("Odd: ",val)
+print(divident_pos(arr, divisior, quotient, rem))
