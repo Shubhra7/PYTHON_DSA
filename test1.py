ac09a0b43f3fe977b25e118370919d5fd9662980
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-25 15:18:12 +0530

    bpre debug

--- test1.py
+++ test1.py
@@ -1,13 +1,26 @@
-import sys
+arr=[9,8,7,5,4,3,2,1,0,-1,-2,3,4,8,6,4,3,2,1,0]
+# arr=[1,6,0,-1,-2,3,4,8,6,4,3,2,1,0]
+# arr=[1]
+count =0
+i=1
+maxi=0
+while i< len(arr):
+    if(arr[i] < arr[i-1]):
+        count += 1
+    else:
+        if count > maxi:
+            maxi = count
+            ro=i
+        count = 0
+    i += 1
 
-arr = [5,4,2,1,2,1,3]
-mini, mini2 = arr[0],arr[0]
+if(count > maxi):
+    maxi=count
+    ro=len(arr)
 
-for i in range(len(arr)):
-    if mini > arr[i]:
-        mini2 = mini
-        mini = arr[i]
-    elif(mini2 > arr[i] and mini != arr[i]):
-        mini2 = arr[i]
-
-print(mini2)
+print("val---")
+print(maxi)
+print(ro)
+print(ro-maxi-1," ",ro)
+print(arr[ro-maxi-1:ro])
+    
