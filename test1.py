724994277b52897bbd8daaad9b412dea1ac61c58
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-27 08:07:59 +0530

    hour pair done

--- test1.py
+++ test1.py
@@ -1,24 +1,19 @@
-def solve_dec(arr):
-    count = 0
-    maxi=0
-    ro=0
-    for i in range(1,len(arr)):
-        if(arr[i] < arr[i-1]):
-            count += 1
-        else:
-            if(maxi < count):
-                maxi = count
-                ro = i-1
-            count = 0
-    if(count > maxi):
-        maxi = count
-        ro = i
-    print(ro)
-    print(maxi+1)
-    print(arr[ro-maxi:ro+1])
-    
+def target(n):
+    return ((n//60) +  1)*60
 
-arr=[1,6,0,-1,-2,3,4,8,6,4,3,2,1,0]
-# arr=[1]
-# arr=[9,8,7,5,4,3,2,1,0,-1,-2,3,4,8,6,4,3,2,1,0]
-solve_dec(arr)
+arr = [2,58,58,2,60,60]
+
+d={}
+l=[]
+count=0
+for i in range(len(arr)):
+    tar = (target(arr[i]) - arr[i])
+    if tar in d and d[tar]>0:
+        l.append([arr[i],tar])
+        d[tar] -= 1
+        count += 1
+    else:
+        d[tar] = d.get(tar,0)+1
+print(d)
+print(count)
+print(l)
