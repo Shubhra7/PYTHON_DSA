40d3a3ff97e3dfc1df4305d181c15a08c973f634
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-27 07:40:24 +0530

    done sweet

--- test1.py
+++ test1.py
@@ -1,13 +1,28 @@
-def special_fibo(n):
-    if(n==0 or n==1):
-        return 1
-    a=1
-    b=1
-    for i in range(2,n+1):
-        c = (a**2)+(b**2)
-        a=b
-        b=c
-    return (b%47)
+def solve(n,e,d):
+    if(e==0 or d==0):
+        return 0
+    if(n==0):
+        return -1
+    if(n*6 < e*7):
+        return -1
+    if(d<7):
+        ans = ((e*d)//n) + ((e*d)%n != 0)
+        return ans
+    weeks = d//7
+    ans = ((weeks*e*7)//n) + ((weeks*e*7)%n != 0)
+    left = (ans*n) - (weeks*7*e)
+    left_days = d%7
+    req = (left_days*e)-left
+    ans += (req//n) + (req%n !=0)
+    return ans
 
-n=int(input("Enter the value of n: "))
-print(special_fibo(n))   
+
+# n=5
+# e=2
+# d=10
+
+n=12
+e=11
+d=7
+
+print(solve(n,e,d))
