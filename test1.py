f9aed650f3ec530f9db7af3848716d73777aaa4c
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-27 08:15:02 +0530

    max match done

--- test1.py
+++ test1.py
@@ -1,19 +1,25 @@
-def target(n):
-    return ((n//60) +  1)*60
+def mat(a,b):
+    i,j = len(a)-1,len(b)-1
+    count=0
+    while (i>=0 and j>=0 and a[i]==b[j]):
+        count+=1
+        i-=1
+        j-=1
+    return count
+arr = ['gender','blender','blunder','under']
+match = 'thunder'
 
-arr = [2,58,58,2,60,60]
-
-d={}
-l=[]
 count=0
-for i in range(len(arr)):
-    tar = (target(arr[i]) - arr[i])
-    if tar in d and d[tar]>0:
-        l.append([arr[i],tar])
-        d[tar] -= 1
-        count += 1
-    else:
-        d[tar] = d.get(tar,0)+1
-print(d)
-print(count)
-print(l)
+maxi=0
+ans=""
+for i in arr:
+    count = mat(i,match)
+    if(count > maxi):
+        maxi=count
+        ans=i
+    if(count==maxi):
+        if(len(ans) > len(i)):
+            ans=i
+
+print(maxi)
+print(ans)
