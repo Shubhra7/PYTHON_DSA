4538bd04ad08cb0531e016cf04690c553872b7de
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-28 00:52:44 +0530

    one of the best recursive linkedlist

--- Reverse_Linked_List.py
+++ Reverse_Linked_List.py
@@ -17,11 +17,30 @@ class LinkedList:
             llprint += str(p.val)+ " "
             p = p.next
         return llprint
+    def reverseList(self,head):
+        if head is None or head.next is None:
+            return head
+        
+        # Reverse the rest list
+        rest = self.reverseList(head.next)
+
+        # Put first element at the end
+        head.next.next = head
+        head.next = None
+
+        # Fix the header pointer
+        return rest
 
 obj = LinkedList()
 obj.push(3)
 obj.push(4)
 obj.push(45)
 
+print("The primary Linekd List is: ")
+print(obj)
+
+obj.head = obj.reverseList(obj.head)
+
+print("The reversed Linked list is: ")
 print(obj)
         
