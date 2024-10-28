aab7afd0dcea498d00236140543f7b0e7a773f6b
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-28 07:57:38 +0530

    decres done

--- Reverse_Linked_List.py
+++ Reverse_Linked_List.py
@@ -1,26 +1,22 @@
-def solve(n,e,d):
-    if(n==0):
-        return -1
-    if(e==0 or d==0):
-        return 0
-    if((n*6) < (e*7)):
-        return -1
-    if(d<7):
-        return ((e*d)//n) + ((e*d)%n != 0)
-    weeks = d//7
-    ans = ((weeks*7*e)//n) + ((weeks*7*e)%n != 0)
-    left = (ans*n) - (weeks*7*e)
-    left_days = d%7
-    req = (left_days*e) - left
-    ans += (req // n) + (req%n != 0)
-    return ans
+arr=[9,8,7,5,4,3,2,1,0,-1,-2,3,4,8,6,4,3,2,1,0]
+# arr=[1,6,0,-1,-2,3,4,8,6,4,3,2,1,0]
+# arr=[1]
+j=0
+count=0
+maxi=0
+l=[]
 
-n=5
-e=2
-d=10
+for i in range(1,len(arr)):
+    if(arr[i] < arr[i-1]):
+        count += 1
+    else:
+        if(maxi < count):
+            maxi = count
+            j=i-1
+        count = 0
+if(maxi < count):
+    maxi = count
+    j= len(arr)-1
 
-# n=12
-# e=11
-# d=7
-
-print(solve(n,e,d))
+print(maxi+1)
+print(arr[j-maxi:j+1])
