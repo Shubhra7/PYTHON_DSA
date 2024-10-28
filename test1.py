bebf9a4db4f790c3133cf025f870ff04fef2f843
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-28 08:15:56 +0530

    del earn

--- test1.py
+++ test1.py
@@ -1,25 +1,16 @@
-def mac(a,b):
-    i,j = len(a)-1, len(b)-1
-    count=0
-    while(i>=0 and j>=0 and a[i]==b[j]):
-        count += 1
-        i-=1
-        j-=1
-    return count
+from collections import Counter
 
-arr = ["gender","blender","blunder","under"]
-match = "thunder"
+def delete_earn(nums):
+    d=Counter(nums)
+    mini = min(nums)
+    maxi = max(nums)
+    pick = mini*d[mini]
+    nopick = 0
+    for i in range(mini+1,maxi+1):
+        newpick = nopick + (i*d[i])
+        newnopick = max(pick,nopick)
 
-maxi=0
-ans=""
-for i in arr:
-    val = mac(i,match)
-    if(val > maxi):
-        maxi = val
-        ans = i
-    if(val == maxi):
-        if(len(i) < len(ans)):
-            ans = i
-
-print(maxi)
-print(ans)
+        pick,nopick = newpick, newnopick
+    return max(pick,nopick)
+nums = [2,2,3,3,3,4]
+print(delete_earn(nums))
