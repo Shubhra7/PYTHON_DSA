9a9ca4ec516500c65d62de18dd73bf462fcf3556
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-28 10:37:28 +0530

    sum prime index done

--- test1.py
+++ test1.py
@@ -1,7 +1,19 @@
-arr = [5,4,2,1,2,1,3]
-s=set(arr)
-l=sorted(s,reverse=True)
-if(len(l) > 1):
-    print(l[1])
-else:
-    print("Not possible")
+def isprime(n):
+    if(n>1 and n<=3):
+        return True
+    else:
+        if((n**2)-1)%24 == 0:
+            return True
+    return False
+
+def prime_index_sum(arr,n):
+    sum=0
+    for i in range(2,len(arr)):
+        if(isprime(i)):
+            sum += arr[i]
+    return sum
+
+
+arr =[10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+n=10
+print(prime_index_sum(arr,n))
