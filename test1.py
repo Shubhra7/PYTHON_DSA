846f0490914aba9cd129a65b82839f727f7a9c34
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-28 09:26:35 +0530

    done

--- test1.py
+++ test1.py
@@ -1,22 +1,11 @@
-s1="adventure"
-s2="fadvenuture"
+def max_profit(price):
+    pick = -price[0]
+    nopick = 0
+    for i in range(1,len(price)):
+        pick = max(pick,nopick-price[i])
+        nopick = max(nopick,pick + price[i])
+    return nopick
 
-row = len(s1)
-col = len(s2)
 
-dp=[[0 for i in range(col+1)]for j in range(row+1)]
-maxi=0
-ro,co = -1,-1
-for i in range(1,row+1):
-    for j in range(1,col+1):
-        if(s1[i-1] == s2[j-1]):
-            dp[i][j] = dp[i-1][j-1]+1
-            if(maxi < dp[i][j]):
-                maxi = dp[i][j]
-                ro,co = i,j
-        else:
-            dp[i][j]=0
-
-print(maxi)
-print(ro,co)
-print(s1[ro-maxi:ro])
+price = [7,1,5,3,6,4]
+print("The maximum profit will be: ",max_profit(price))
