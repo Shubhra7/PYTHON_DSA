5ca12751f11349939f64b3b1afc9584a2c366ae7
Shubrha <shubhrajit.g2002@gmail.com>
2024-10-28 10:02:29 +0530

    done

--- test1.py
+++ test1.py
@@ -1,10 +1,7 @@
-def win_joshep(n,k):
-    if(n==1):
-        return 0
-    else:
-        return (((win_joshep(n-1,k))+k)%n)
-
-
-print(win_joshep(41,2)+1)
-print(win_joshep(5,3)+1)
-print(win_joshep(1,3)+1)
+arr = [5,4,2,1,2,1,3]
+s=set(arr)
+l=sorted(s,reverse=True)
+if(len(l) > 1):
+    print(l[1])
+else:
+    print("Not possible")
