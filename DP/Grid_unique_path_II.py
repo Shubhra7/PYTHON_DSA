0221e787139f0f8be7ea87a4a013541cd2ab8f51
Shubrha <shubhrajit.g2002@gmail.com>
2024-11-24 11:54:21 +0530

    Tbulation done

--- DP/Grid_unique_path_II.py
+++ DP/Grid_unique_path_II.py
@@ -23,6 +23,11 @@ The testcases are generated so that the answer will be less than or equal to 2 *
 # 1. Right -> Right -> Down -> Down
 # 2. Down -> Down -> Right -> Right
 
+
+#-----------------------------
+# Using Recursion 
+#-----------------------------
+
 def find_uni_path(m,n,arr,dp):
     if(m>=0 and n>=0 and arr[m][n]==1): # for avoiding the obstacle
         return 0
@@ -37,12 +42,34 @@ def find_uni_path(m,n,arr,dp):
     dp[m][n]=up + right
     return dp[m][n]
 
-# obstacleGrid = [[0,0,0],
-#                 [0,1,0],
-#                 [0,0,0]]
+obstacleGrid = [[0,0,0],
+                [0,1,0],
+                [0,0,0]]
 
-obstacleGrid = [[0,1],[0,0]]
+# obstacleGrid = [[0,1],[0,0]]
 
 m,n = len(obstacleGrid), len(obstacleGrid[0])
 dp= [[-1 for i in range(n)] for j in range(m)]
 print(find_uni_path(m-1,n-1,obstacleGrid,dp))
+
+
+#-----------------------------
+# Using Tabulation
+#-----------------------------
+
+dp=[[0 for i in range(n)] for j in range(m)]
+
+for i in range(m):
+    for j in range(n):
+        if(i==0 and j==0):  # staring point
+            dp[i][j]=1
+        elif( i>=0 and j>=0 and obstacleGrid[i][j]==1):  # obstacle trackel
+            dp[i][j]=0
+        else:
+            up,down = 0,0
+            if(i>0):
+                up = dp[i-1][j]
+            if(j >0):
+                down = dp[i][j-1]
+            dp[i][j] = up+down
+print(dp[m-1][n-1])
