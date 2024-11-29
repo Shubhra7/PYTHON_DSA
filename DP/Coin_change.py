ac7ddcbde44f9676d354aeb7fd110ac2855ae796
Shubrha <shubhrajit.g2002@gmail.com>
2024-11-30 00:18:19 +0530

    tabulation done

--- DP/Coin_change.py
+++ DP/Coin_change.py
@@ -41,23 +41,54 @@ Output: 3
 #------------------------------------
 
 import sys
-def min_coin_need(ind,coins,amount):
+def min_coin_need(ind,coins,amount,dp):
     if(ind==0):
         if(amount % coins[0]==0):       # Last coin case
             return amount//coins[0]
         else:
             return sys.maxsize
-    notake = 0 + min_coin_need(ind-1,coins,amount)
+    if(dp[ind][amount] != -1):
+        return dp[ind][amount]
+    notake = 0 + min_coin_need(ind-1,coins,amount,dp)
     take=sys.maxsize
     if(amount >= coins[ind]):
-        take = 1 + min_coin_need(ind,coins,amount-coins[ind])
-    return min(notake,take)
+        take = 1 + min_coin_need(ind,coins,amount-coins[ind],dp)
+    dp[ind][amount]=min(notake,take)
+    return dp[ind][amount]
     
 coins=[1,2,5]
 amount=11
 n=len(coins)
-ans = min_coin_need(n-1,coins,amount)
+dp=[[-1 for i in range(amount+1)]for j in range(n)]
+ans = min_coin_need(n-1,coins,amount,dp)
 if ans>=sys.maxsize:
     print(-1)
 else:
     print(ans)
+
+
+
+#------------------------------------
+#       Tabulation Method
+#------------------------------------
+
+coins=[1,2,5]
+amount=11
+n=len(coins)
+
+dp=[[-1 for i in range(amount+1)]for j in range(n)]
+
+for i in range(amount+1):
+    if(i%coins[0] == 0):
+        dp[0][i] = i//coins[0]
+    else:
+        dp[0][i] = sys.maxsize
+
+for i in range(1,n):
+    for j in range(amount+1):
+        notake = 0+dp[i-1][j]
+        take = sys.maxsize
+        if(j >= coins[i]):
+            take = 1 + dp[i][j-coins[i]]
+        dp[i][j]=min(notake,take)
+print(dp[n-1][amount])
