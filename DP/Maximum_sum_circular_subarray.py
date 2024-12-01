3301bf9298433153c6e24a7cff68153c0bfbd0d6
Shubrha <shubhrajit.g2002@gmail.com>
2024-12-01 09:55:49 +0530

    done

--- DP/Maximum_sum_circular_subarray.py
+++ DP/Maximum_sum_circular_subarray.py
@@ -1,3 +1,6 @@
+# video : https://youtu.be/Q1TYVUEr-wY?si=0xR3Om4BqJq4W01Z
+# link: https://leetcode.com/problems/maximum-product-subarray/description/
+
 """
 Given a circular integer array nums of length n, return the maximum possible sum of a non-empty subarray of nums.
 A circular array means the end of the array connects to the beginning of the array. Formally, the next element of nums[i] is nums[(i + 1) % n] and the previous element of nums[i] is nums[(i - 1 + n) % n].
