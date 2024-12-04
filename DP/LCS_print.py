fdbfa47ed328f98890453745f66d1aa2a7a7087b
Shubrha <shubhrajit.g2002@gmail.com>
2024-12-04 13:41:13 +0530

    starting is important

--- DP/LCS_print.py
+++ DP/LCS_print.py
@@ -8,7 +8,7 @@ def print_max_LCS(word1, word2):
     n,m  = len(word1), len(word2)
     dp = [[0]*(m+1) for _ in range(n+1)]
     
-    for i in range(1, n+1):
+    for i in range(1, n+1):         #  ****Straing index from 1 is very important****
         for j in range(1, m+1):
             if word1[i-1] == word2[j-1]:
                 dp[i][j] = dp[i-1][j-1] + 1
