a6c37249955a8d49892f2a1b12f506f4dba44ecc
Shubrha <shubhrajit.g2002@gmail.com>
2024-12-06 18:32:25 +0530

    start pattern matching in kmp

---
+++ DP/LPS_cases/Pattern_match_KMP_Algo.py
@@ -0,0 +1,13 @@
+# https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
+
+
+# We use LPS because it would be like mirror image
+# so if the last matches element from the second side we got then after that we can check
+# Like mota vai video [match ........... match ............ match]
+
+"""
+txt = "aabaacaadaabaaba"
+pat = "aaba"
+
+o/p == > 0 9 12 
+"""
