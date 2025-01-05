85365160b3497e75bf6e8f2a8876d171b66ed270
Shubrha <shubhrajit.g2002@gmail.com>
2025-01-06 00:46:19 +0530

    group anargram test done

--- Recursion/test.py
+++ Recursion/test.py
@@ -1,12 +1,47 @@
-from collections import Counter
-def countPairs(arr, target):
-    #Your code here
-    dp={}
-    count=0
-    for i in range(len(arr)):
-        tar = target-arr[i]
-        count += dp.get(tar,0)
-        dp[arr[i]] = dp.get(arr[i],0)+1
-    return count
+MAX_CHAR = 26
 
-print(countPairs([5,6,5,7,7,8],13))
+# function to generate hash of word s
+def getHash(s):
+    hashList = []
+    freq = [0] * MAX_CHAR
+    
+    # Count frequency of each character
+    for ch in s:
+        freq[ord(ch) - ord('a')] += 1
+    
+    # Append the frequency to construct the hash
+    for i in range(MAX_CHAR):
+        hashList.append(str(freq[i]))
+        hashList.append("$")
+    print(''.join(hashList))
+    return ''.join(hashList)
+
+def anagrams(arr):
+    res = []
+    mp = {}
+    
+    for i in range(len(arr)):
+        key = getHash(arr[i])
+        
+        # If key is not present in the hash map, add
+        # an empty group (list) in the result and
+        # store the index of the group in hash map
+        if key not in mp:
+            mp[key] = len(res)
+            res.append([])
+        
+        # Insert the string in its correct group
+        res[mp[key]].append(arr[i])
+        print(res)
+        print(mp)
+    
+    return res
+
+if __name__ == "__main__":
+    arr = ["act", "god", "cat", "dog", "tac"]
+    
+    res = anagrams(arr)
+    for group in res:
+        for word in group:
+            print(word, end=" ")
+        print()
