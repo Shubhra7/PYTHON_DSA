1ea7e818efeafeeded76c417f3daba69af27197e
Shubrha <shubhrajit.g2002@gmail.com>
2025-01-14 17:07:02 +0530

    brute force done

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,18 +1,19 @@
 def find_sum(arr,tar):
     n=len(arr)
-    ind=[0,n+1]
+    ind=[-1,-1]
     for i in range(n):
         ans=0
         for j in range(i,n):
             ans += arr[j]
-            print(ans)
+            # print(ans)
             if ans==tar:
-                print("Bro")
-                if ind[1]-ind[0] >(j-i):
-                    ind[0],ind[1]=i,j
-        print("hello ",i,"  ",ind)
-    return ind
+                return [i+1,j+1]
+    if ind[0]==-1:
+        return [-1]
+    else:
+        return ind
 
 arr=[1,2,3,7,5]
 print(find_sum(arr,12))
-# print(find_sum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10],15))
+print(find_sum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10],15))
+print(find_sum([5,3,4],2))
