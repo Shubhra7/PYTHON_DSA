2636222f470904856d2b0e75fd47a9e253ae80f7
Shubrha <shubhrajit.g2002@gmail.com>
2025-01-14 18:21:41 +0530

    second test done let's do

--- Competitive/test.py
+++ Competitive/test.py
@@ -1,19 +1,14 @@
-def find_sum(arr,tar):
-    n=len(arr)
-    ind=[-1,-1]
-    for i in range(n):
-        ans=0
-        for j in range(i,n):
-            ans += arr[j]
-            # print(ans)
-            if ans==tar:
-                return [i+1,j+1]
-    if ind[0]==-1:
-        return [-1]
-    else:
-        return ind
+def find_sum(arr,sum):
+    h={}
+    currentSum=0
+    for i in range(len(arr)):
+        currentSum += arr[i]
+        if currentSum == sum:
+            return [0,i]
+        if h.get(currentSum-sum,-1) != -1:
+            return [h[currentSum-sum]+1,i]
+        h[currentSum]=i
+    return [-1,-1]
 
-arr=[1,2,3,7,5]
-print(find_sum(arr,12))
-print(find_sum([1, 2, 3, 4, 5, 6, 7, 8, 9, 10],15))
-print(find_sum([5,3,4],2))
+# print(find_sum([-4,-6,-1,1],-10))
+print(find_sum([-1,0,-1,0],0))
