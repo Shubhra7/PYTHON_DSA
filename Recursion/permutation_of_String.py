b2f1ec1817f6d81e1fea2c517aab1a07cd0d0b5c
Shubrha <shubhrajit.g2002@gmail.com>
2025-02-02 11:55:20 +0530

    apporach 2 done

--- Recursion/permutation_of_String.py
+++ Recursion/permutation_of_String.py
@@ -1,5 +1,23 @@
 from copy import deepcopy
 
+# https://www.youtube.com/watch?v=f2ic2Rsc9pU
+#----------------------------------
+#      Appoarch= 2     Using Swap
+#----------------------------------
+def recurPermute_Appr_2(index,sarr,ans):
+    if index==len(sarr):
+        ans.append(deepcopy("".join(sarr)))
+        return
+    for i in range(index,len(sarr)):
+        sarr[index],sarr[i] = sarr[i],sarr[index] # swao
+        recurPermute_Appr_2(index+1,sarr,ans)
+        sarr[index],sarr[i] = sarr[i],sarr[index] #for thik kore in previous version
+
+#https://www.youtube.com/watch?v=YK78FU5Ffjw
+#----------------------------------
+#      Appoarch= 1     Using map
+#----------------------------------
+
 def recurPermute(sarr, ds, ans, freq):
     if len(ds) == len(sarr):
         ans.append(deepcopy("".join(ds)))
@@ -19,5 +37,6 @@ ans = []
 ds = []
 freq = [False] * len(sarr)  # Boolean list
 
-recurPermute(sarr, ds, ans, freq)
+# recurPermute(sarr, ds, ans, freq)
+recurPermute_Appr_2(0,sarr,ans)
 print(ans)
