94a9142839a328be198a8d32459bb4013ec6424e
Shubrha <shubhrajit.g2002@gmail.com>
2025-02-22 01:22:34 +0530

    hmm

--- Tree/test.py
+++ Tree/test.py
@@ -1,33 +1,44 @@
-from collections import deque
-
-q= deque()
-visited = set()
-directions = [[1,0],[0,1],[-1,0],[0,-1],[1,1],[-1,-1]]
-
-def bfs(i,j):
-    q.append((i,j))
-    visited.add((i,j))
-    while q:
-        ro, co = q.popleft()
-        for m,n in directions:
-            nro, ncol = ro+m, co+n
-            if(nro in range(row) and ncol in range(col) and 
-               grid[nro][ncol]==1 and (nro,ncol) not in visited):
-                visited.add((nro,ncol))
-                q.append((nro,ncol))
-
-grid = [[1,1,1,0,1,0,1],
-        [1,1,0,0,1,0,1],
-        [1,0,1,1,1,0,0]]
-
-row = len(grid)
-col = len(grid[0])
-cnt =0 
-
-for i in range(row):
-    for j in range(col):
-        if(grid[i][j]==1 and (i,j) not in visited):
-            bfs(i,j)
-            cnt += 1
-
-print("The total island is: ", cnt)
+import copy
+def subseq(ind,arr,ds,ans):
+    if ind == len(arr):
+        if len(ds)==0:
+            return
+        val=1
+        for i in ds:
+            val *= i
+        # ans.append(copy.deepcopy(ds))
+        ans.append(copy.deepcopy(val))
+        return
+    ds.append(arr[ind])
+    subseq(ind+1,arr,ds,ans)
+    ds.pop()
+    subseq(ind+1,arr,ds,ans)
+def isPrime(n):
+    if n>1 and n<=3:
+        return True
+    if ((n**2)-1)%24==0:
+        return True
+    return False
+def check_beau(n):
+    if n==2:
+        return True
+    for i in range(2,n):
+        if n%i==0:
+            if not isPrime(i):
+                return False
+    return True
+        
+arr=[10,6,1]
+# arr=[3]
+ans=[]
+ds=[]
+subseq(0,arr,ds,ans)
+print("kalu ",ans)
+finalans=[]
+for i in ans:
+    if i!=1:
+        if check_beau(i):
+            print(i,"---HI")
+            finalans.append(i)
+print(finalans)
+print(len(finalans))
--- test1.py
+++ test1.py
@@ -1,28 +1,15 @@
-def A_Wins(s):
-    word=""
-    a,b=[],[]
-    flag=0
-    for i in range(len(s)):
-        word+= s[i]
-        if(word == "snake" or word == "water" or word == "gun"):
-            if(flag == 0):
-                a.append(word[0])
-                flag=1
-            else:
-                b.append(word[0])
-                flag=0
-            word=""
-    ans=0
-    for i in range(len(a)):
-        if (a[i]=='s' and b[i]=='w') or (a[i]=='w' and b[i]=='g') or (a[i]=='g' and b[i]=='s'):
-            ans += 1
-    
-    return ans
+import copy
+def subseq(ind,arr,ds,ans):
+    if ind==len(arr):
+        ans.append(copy.deepcopy(ds))
+        return
+    ds.append(arr[ind])
+    subseq(ind+1,arr,ds,ans)
+    ds.pop()
+    subseq(ind+1,arr,ds,ans)
 
-
-s="snakewatergunsnake"
-print("A wins: ",A_Wins(s)," times.")
-print()
-
-s1="snakesnakewatergun"
-print("A wins: ",A_Wins(s1)," times.")
+arr=[10,6,1]
+ans=[]
+ds=[]
+subseq(0,arr,ds,ans)
+print(ans)
