baf2d62a27f3883a4b13c621e73705050e4c6765
Shubrha <shubhrajit.g2002@gmail.com>
2025-02-25 15:40:07 +0530

    done compute ftp

--- Competitive/Median_of_2_sorted_array.py
+++ Competitive/Median_of_2_sorted_array.py
@@ -59,9 +59,10 @@ def findMedianSortedArrays(nums1, nums2):
             high=mid1-1
         else:
             low=mid1+1
-    return -1
+    # return -1
 
 print(findMedianSortedArrays([1,2],[3,4]))   #2.5
 print(findMedianSortedArrays([1,3],[2]))   #2
+print(findMedianSortedArrays([1,2,3,4,6],[5]))   #2
 
     
