c577806ea65b675d56566574ac5d5f8133aa2133
Shubrha <shubhrajit.g2002@gmail.com>
2025-03-12 18:50:06 +0530

    Mancher Algorithm done

---
+++ DP/longest_palindrome_substring(Manchers Algo).py
@@ -0,0 +1,29 @@
+"""
+Example 1:
+
+Input: s = "babad"
+Output: "bab"
+Explanation: "aba" is also a valid answer.
+Example 2:
+
+Input: s = "cbbd"
+Output: "bb"
+
+example 3:
+"aacabdkacaa"
+o/p==> 'aca'
+"""
+
+# Normal reverse and do LCS will not work, it only worked for subsequences
+# Mancher Algorithm done
+def expan(left,right,s):
+    while left>=0 and right<len(s) and s[left]==s[right]:
+        left-=1
+        right+=1
+    return s[left+1:right]
+
+s="aacabdkacaa"
+res=""
+for i in range(len(s)):
+    res=max(res,expan(i,i,s),expan(i,i+1,s),key=len)
+print(res)
