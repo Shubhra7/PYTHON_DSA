8b75d52da74588f8de78a798d0daf76fee2e993c
Shubrha <shubhrajit.g2002@gmail.com>
2025-03-12 18:20:05 +0530

    Longest palindrome in a String done

---
+++ testing/accen.py
@@ -0,0 +1,26 @@
+def LCS(s1,s2):
+    n,m=len(s1),len(s2)
+    dp=[[0]*(m+1) for _ in range(n+1)]
+    maxi=0
+    ro,co=-1,-1
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            if s1[i-1]==s2[j-1]:
+                dp[i][j]=dp[i-1][j-1]+1
+                if maxi<dp[i][j]:
+                    maxi=dp[i][j]
+                    ro,co=i,j
+            else:
+                dp[i][j]=0
+    ans=""
+    for i in range(ro,ro-maxi,-1):
+        ans=ans+s1[i-1]
+    print(ans)
+    return maxi
+
+
+s = 'abc'
+# s = 'forgeeksskeegfor'
+s1= s[::-1]
+ans=LCS(s,s1)
+print(ans)
