bd3e309234b9aae7514057f2ce6fc028bbfc5de6
Shubrha <shubhrajit.g2002@gmail.com>
2025-03-17 16:32:29 +0530

    tabulation method done

--- DP/Edit_distance.py
+++ DP/Edit_distance.py
@@ -18,9 +18,35 @@ rorse -> rose (remove 'r')
 rose -> ros (remove 'e')
 """
 
+
+#################################################
+#     Tabulation method
+#################################################
+class Solution1:
+    def minDistance1(self,word1,word2):
+        def f(n,m):
+            dp=[[0]*(m+1) for _ in range(n+1)]
+            #for base case
+            for i in range(n+1):
+                dp[i][0]=i
+            for j in range(m+1):
+                dp[0][j]=j
+            for i in range(1,n+1):
+                for j in range(1,m+1):
+                    if word1[i-1]==word2[j-1]:
+                        dp[i][j]=dp[i-1][j-1]
+                    else:
+                        dp[i][j]=1+min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])
+            return dp[n][m]
+        n,m=len(word1),len(word2)
+        ans=f(n,m)
+        return ans
+        
+ 
 #################################################
 #     Recusrsion + memozitation
 #################################################
+
 # https://youtu.be/fJaKO8FbDdo
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
@@ -45,3 +71,6 @@ class Solution:
         
 obj=Solution()
 print(obj.minDistance("horse","ros"))  #o/p==>3
+obj1=Solution1()
+print(obj1.minDistance1("horse","ros"))  #o/p==>3
+print(obj1.minDistance1("intention","execution"))  #o/p==>5
