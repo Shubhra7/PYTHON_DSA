290ff16c5344a2943f8ea5be9e78c6e3a782f937
Shubrha <shubhrajit.g2002@gmail.com>
2025-03-18 14:20:32 +0530

    knapscak tabulation method done

--- DP/0_1_knapsack.py
+++ DP/0_1_knapsack.py
@@ -1,5 +1,35 @@
 import sys
 
+
+########################################
+#       Tabulation
+########################################
+
+class Solution:
+    def knapsack(self,maxiW,val,wt):
+        n=len(wt)
+        dp=[[0]*(maxiW+1) for _ in range(n)]
+        for i in range(wt[0],maxiW+1):  # Important line
+            dp[0][i]=val[0]
+        for i in range(1,n):
+            for j in range(maxiW+1):
+                notake=0+dp[i-1][j]
+                take=-sys.maxsize
+                if j>=wt[i]:    # need check to avoid the impossible cases in take branches
+                    take=val[i]+dp[i-1][j-wt[i]]
+                dp[i][j]=max(take,notake)
+        return dp[n-1][maxiW]
+
+
+maxiW=4
+val=[1,2,3]  #o/p==>3
+wt=[4,5,1]
+
+# maxiW=3
+# val=[1,2,3]   #o/p==>0
+# wt=[4,5,6]
+obj=Solution()
+print(obj.knapsack(maxiW,val,wt))
 ########################################
 #       Recursion + memo
 ########################################
