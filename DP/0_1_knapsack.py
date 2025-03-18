b86c1e4cd4b7940f2bf65e326aa3327ef3118e2c
Shubrha <shubhrajit.g2002@gmail.com>
2025-03-18 14:15:40 +0530

    done memorization method

--- DP/0_1_knapsack.py
+++ DP/0_1_knapsack.py
@@ -1,27 +1,34 @@
 import sys
+
+########################################
+#       Recursion + memo
+########################################
 class Solution:
     def knapsack(self,maxiW,val,wt):
-        def f(ind,maxiW):
+        def f(ind,maxiW,dp):
             if ind==0:
                 if maxiW>=wt[0]:
                     return val[0]
                 return 0
-            notake=0+f(ind-1,maxiW)
+            if dp[ind][maxiW]!=-1:
+                return dp[ind][maxiW]
+            notake=0+f(ind-1,maxiW,dp)
             take=-sys.maxsize
             if wt[ind]<=maxiW:
-                take=val[ind]+f(ind-1,maxiW-wt[ind])
+                take=val[ind]+f(ind-1,maxiW-wt[ind],dp)
             return max(take,notake)
         n=len(wt)
-        ans=f(n-1,maxiW)
+        dp=[[-1]*(maxiW+1) for _ in range(n)]
+        ans=f(n-1,maxiW,dp)
         return ans
         
 
-maxiW=4
-val=[1,2,3]  #o/p==>3
-wt=[4,5,1]
+# maxiW=4
+# val=[1,2,3]  #o/p==>3
+# wt=[4,5,1]
 
-# maxiW=3
-# val=[1,2,3]   #o/p==>0
-# wt=[4,5,6]
+maxiW=3
+val=[1,2,3]   #o/p==>0
+wt=[4,5,6]
 obj=Solution()
 print(obj.knapsack(maxiW,val,wt))
