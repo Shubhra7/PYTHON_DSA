1540882377b519645f2ae42855d4a0ed49026ca3
Shubrha <shubhrajit.g2002@gmail.com>
2025-03-21 12:11:13 +0530

    dijikstra algo

---
+++ Tree/Graph/Dijikstra_Algo.py
@@ -0,0 +1,25 @@
+import heapq
+
+def dijk(v,adj,source):
+    
+graph=   [  [0, 4, 0, 0, 0, 0, 0, 8, 0],
+            [4, 0, 8, 0, 0, 0, 0, 11, 0],
+            [0, 8, 0, 7, 0, 4, 0, 0, 2],
+            [0, 0, 7, 0, 9, 14, 0, 0, 0],
+            [0, 0, 0, 9, 0, 10, 0, 0, 0],
+            [0, 0, 4, 14, 10, 0, 2, 0, 0],
+            [0, 0, 0, 0, 0, 2, 0, 1, 6],
+            [8, 11, 0, 0, 0, 0, 1, 0, 7],
+            [0, 0, 2, 0, 0, 0, 6, 7, 0]
+        ]
+v=len(graph)
+adj=[[] for _ in range(v)]
+for i in range(v):
+    for j in range(v):
+        if graph[i][j]!=0:
+            adj[i].append((j,graph[i][j]))
+print(adj)
+source=0
+print(dijk(v,adj,source))
+
+
