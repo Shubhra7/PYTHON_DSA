8c41ade410b72c10ee11fbab2f9aff05a1522a72
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-03-29 08:52:34 +0530

    Let's start

---
+++ DP/Partition dp/Matrix_chain_multi.py
@@ -0,0 +1,16 @@
+# https://youtu.be/vRVfmbCFW7Y
+# https://www.naukri.com/code360/problems/matrix-chain-multiplication_975344?leftPanelTabValue=PROBLEM
+
+"""
+I/P==
+4
+4 5 3 2
+
+O/P ==> 70
+"""
+
+n=int(input())
+arr=list(map(int,input().strip().split(' ')))
+
+print(n)
+print(arr)
