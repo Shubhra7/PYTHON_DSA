b96047054f3cca47af46b7deec795b38a9bf4c92
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-03-31 00:00:49 +0530

    let's tcs

--- test.py
+++ test.py
@@ -1,11 +1,4 @@
-a=[[1,2,3],
-   [4,5,6],
-   [7,8,9]]
-
-n=len(a)
-for i in range(n):
-    for j in range(i+1):
-        a[i][j],a[j][i]=a[j][i],a[i][j]
-
-for i in a:
+a=int(input())
+mat=[list(map(int,input().strip().split())) for _ in range(a)]
+for i in mat:
     print(i)
