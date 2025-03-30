c706471bb42f5358a225216b9a6482fa9f7d7214
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-03-30 23:59:36 +0530

    rotate matrix done

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-arr=[[1,2],[2,1],[3,-1]]
-arr.sort(key=lambda x:x[1])
-print(arr)
-del arr[1]
-print(arr)
+a=[[1,2,3],
+   [4,5,6],
+   [7,8,9]]
+
+n=len(a)
+for i in range(n):
+    for j in range(i+1):
+        a[i][j],a[j][i]=a[j][i],a[i][j]
+
+for i in a:
+    print(i)
