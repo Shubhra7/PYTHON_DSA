5f655e08c4efe7b5024be1ac9f39072b7fb6dbf7
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-03-30 08:04:15 +0530

    LCS test done

--- DP/testdp.py
+++ DP/testdp.py
@@ -1,24 +1,30 @@
-# import sys
-# data=sys.stdin.read().split()
-# print(data)
-# arr=list(map(int,data))
-# print(arr)
+# adventure xyzadvklptu
+# Highest common subsequences length:  5
+# The Largest Common subsequence is:  advtu
 
-# import re
-# arr="Ramu96 jau Kalu#**"
-# arr=re.sub(r"[^A-Za-z0-9]","",arr)
-# print(arr)
+s1,s2=map(str,input().strip().split())
+# print(s1," ",s2)
+n,m=len(s1),len(s2)
+dp=[[0]*(m+1) for _ in range(n+1)]
+for i in range(1,n+1):
+    for j in range(1,m+1):
+        if s1[i-1]==s2[j-1]:
+            dp[i][j]=dp[i-1][j-1]+1
+        else:
+            dp[i][j]=max(dp[i-1][j],dp[i][j-1])
+print(dp[n][m])
 
-import re
-arr = "Ramur*96 jau KaluR#**"
-arr = re.sub(r"[Rr][^A-Za-z0-9]+", "", arr)
-print(arr)
-arr=re.sub("\s+","",arr)
-print(arr)
-
-
-
-arr=[1,2,3,4]
-p=[2,4]
-arr.extend(p)
-# print(arr)
+ans=""
+i=n
+j=m
+while i>0 and j>0:
+    if s1[i-1]==s2[j-1]:
+        ans=s1[i-1]+ans
+        i-=1
+        j-=1
+    else:
+        if dp[i-1][j]>dp[i][j-1]:
+            i=i-1
+        else:
+            j=j-1
+print(ans)
