749a2dccd4ea1b7b4179023e9ea1ff90c0aa5af3
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-03-30 12:54:18 +0530

    done buggy

--- test1.py
+++ test1.py
@@ -1,15 +1,18 @@
-import copy
-def subseq(ind,arr,ds,ans):
-    if ind==len(arr):
-        ans.append(copy.deepcopy(ds))
-        return
-    ds.append(arr[ind])
-    subseq(ind+1,arr,ds,ans)
-    ds.pop()
-    subseq(ind+1,arr,ds,ans)
-    
-arr=[10,6,1]
-ans=[]
-ds=[]
-subseq(0,arr,ds,ans)
-print(ans)
+# n=2
+# mat1=[list(map(int,input().split()))for i in range(n)]
+# print(mat1)
+# print("Enter the second matrix: ")
+# mat2=[list(map(int,input().split())) for i in range(n)]
+# dp=[[0]*2 for _ in range(2)]
+# for i in range(2):
+#     for j in range(2):
+#         sumi=0
+#         for k in range(2):
+#             sumi+=mat1[i][k]*mat2[k][j]
+#         dp[i][j]=sumi
+# for i in dp:
+#     print(i)
+
+a=['1','2','3']
+p=list(map(int,a))
+print(type(p[1]))
