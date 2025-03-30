bde2a3d992b1640e1d3b78d69adf5ac6510424ad
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-03-30 08:21:30 +0530

    lsb done

--- DP/testdp.py
+++ DP/testdp.py
@@ -1,30 +1,20 @@
-# adventure xyzadvklptu
-# Highest common subsequences length:  5
-# The Largest Common subsequence is:  advtu
-
-s1,s2=map(str,input().strip().split())
-# print(s1," ",s2)
+s1,s2=map(str,input().split())
 n,m=len(s1),len(s2)
-dp=[[0]*(m+1) for _ in range(n+1)]
+dp=[[-1]*(m+1) for _ in range(n+1)]
+maxi=0
+ro,co=-1,-1
 for i in range(1,n+1):
     for j in range(1,m+1):
         if s1[i-1]==s2[j-1]:
             dp[i][j]=dp[i-1][j-1]+1
+            if maxi<dp[i][j]:
+                maxi=dp[i][j]
+                ro,co=i,j
         else:
-            dp[i][j]=max(dp[i-1][j],dp[i][j-1])
-print(dp[n][m])
-
+            dp[i][j]=0
+print(maxi)
 ans=""
-i=n
-j=m
-while i>0 and j>0:
-    if s1[i-1]==s2[j-1]:
-        ans=s1[i-1]+ans
-        i-=1
-        j-=1
-    else:
-        if dp[i-1][j]>dp[i][j-1]:
-            i=i-1
-        else:
-            j=j-1
+for i in range(ro,ro-maxi,-1):
+    ans=s1[i-1]+ans
 print(ans)
+    
