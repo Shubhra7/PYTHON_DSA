8cd7238492fcfc1ff71744c1ee541bdbabae3344
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-03-30 07:56:41 +0530

    Testing done

--- DP/testdp.py
+++ DP/testdp.py
@@ -1,29 +1,24 @@
-class Solution:
-    def isMatch(self, s: str, p: str) -> bool:
-        def f(i,j,dp):
-            if i<0 and j<0:
-                return True
-            if i<0 and j>=0:
-                return False
-            if i>=0 and j<0:
-                for k in range(0,i+1):
-                    if p[k]!='*':
-                        return False
-                return True
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            if p[i]==s[j] or p[i]=='?':
-                dp[i][j]= f(i-1,j-1,dp) 
-                return dp[i][j]
-            if p[i]=='*':  #Main Trick place
-                dp[i][j]=f(i-1,j,dp) or f(i,j-1,dp) #Either * like noting or any length 
-                return dp[i][j]
-            dp[i][j]= False
-            return dp[i][j]
-        n=len(p)
-        m=len(s)
-        dp=[[-1]*(m) for _ in range(n)]
-        ans=f(n-1,m-1,dp)
-        return ans
-obj=Solution()
-print(obj.isMatch("abcdefhjo","**"))
+# import sys
+# data=sys.stdin.read().split()
+# print(data)
+# arr=list(map(int,data))
+# print(arr)
+
+# import re
+# arr="Ramu96 jau Kalu#**"
+# arr=re.sub(r"[^A-Za-z0-9]","",arr)
+# print(arr)
+
+import re
+arr = "Ramur*96 jau KaluR#**"
+arr = re.sub(r"[Rr][^A-Za-z0-9]+", "", arr)
+print(arr)
+arr=re.sub("\s+","",arr)
+print(arr)
+
+
+
+arr=[1,2,3,4]
+p=[2,4]
+arr.extend(p)
+# print(arr)
