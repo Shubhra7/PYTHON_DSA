d4e550223b8bc944e48d4035e4270ae4e1d16bcf
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-04-16 20:22:17 +0530

    dijikstra testing for gfg done

--- test.py
+++ test.py
@@ -1,16 +1,33 @@
-def permut(ind,arr,ans):
-    if ind==len(arr)-1:
-        ans.append("".join(arr))
-        return
-    for i in range(ind,len(arr)):
-        arr[i],arr[ind]=arr[ind],arr[i]
-        permut(ind+1,arr,ans)
-        arr[i],arr[ind]=arr[ind],arr[i]
+import heapq
+class Solution:
+    # Returns shortest distances from src to all other vertices
+    def dij(self,adj,src):
+        pq=[]
+        heapq.heappush(pq,(0,src))
+        dist=[float('inf')]*len(adj)
+        dist[src]=0
+        while pq:
+            dis,node = heapq.heappop(pq)
+            for adjNode,edgeWeight in adj[node]:
+                if dis+edgeWeight < dist[adjNode]:
+                    dist[adjNode]=dis+edgeWeight
+                    heapq.heappush(pq,(dist[adjNode],adjNode))
+        return dist
+    def dijkstra(self, V, edges, src):
+        # code here
+        adj=[[] for _ in range(V)]
+        for i in edges:
+            print(i[0]," ",i[1]," ",i[2])
+            adj[i[0]].append([i[1],i[2]])
+            adj[i[1]].append([i[0],i[2]])
+            # print(adj)
+        for i in adj:
+            print(i)
+        return self.dij(adj,src)
     
+V = 3
+edges= [[0, 1, 1], [1, 2, 3], [0, 2, 6]]
+src = 2
 
-
-s = "ABC"
-ans=[]
-arr=list(s)
-permut(0,arr,ans)
-print(ans)
+obj=Solution()
+print(obj.dijkstra(V,edges,src))
