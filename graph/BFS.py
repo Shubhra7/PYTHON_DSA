47499b90613b05f9dc31e82317a241470befb5e2
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-06-07 10:25:28 +0530

    BFS traversal done

--- graph/BFS.py
+++ graph/BFS.py
@@ -1,1 +1,23 @@
-# https://www.geeksforgeeks.org/problems/depth-first-traversal-for-a-graph/1
+# https://www.geeksforgeeks.org/problems/bfs-traversal-of-graph/1from collections import deque
+
+from collections import deque
+def bfs(node,adj,visit,ans):
+    visit[node]=1
+    q=deque()
+    q.append(node)
+    while q:
+        nd=q.popleft()
+        ans.append(nd)
+        for i in adj[nd]:
+            if visit[i]==0:
+                visit[i]=1
+                q.append(i)
+
+
+
+adj=[[2,3,1],[0],[0,4],[0],[2]]
+ans=[]
+V=len(adj)
+visit=[0]*V
+bfs(0,adj,visit,ans)
+print("The BFS traversal answer is: ",ans)
