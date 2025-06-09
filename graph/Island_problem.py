4152887c9cd3dd5908ccaf82858f9e0dc98525f1
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-06-09 11:42:26 +0530

    Add initial implementation of Island problem with example input and output

---
+++ graph/Island_problem.py
@@ -0,0 +1,12 @@
+# https://www.youtube.com/watch?v=muncqlKJrH0&list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&index=8
+# https://leetcode.com/problems/number-of-islands/description/
+
+"""
+Input: grid = [
+  ["1","1","0","0","0"],
+  ["1","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
+Output: 3
+"""
