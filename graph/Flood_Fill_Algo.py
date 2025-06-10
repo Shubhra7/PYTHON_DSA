0cb5b7a36ac0c351b8407c9b70a198a188cb29a4
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-06-10 22:14:05 +0530

    by BFS done

--- graph/Flood_Fill_Algo.py
+++ graph/Flood_Fill_Algo.py
@@ -9,6 +9,37 @@ Output: [[2,2,2],[2,2,0],[2,0,1]]
 """
 
 ############################################
+#       DONE by BFS
+############################################
+from collections import deque
+class Solution:
+    def floodFill(self, image, sr, sc, color):
+        if image[sr][sc]==color:
+            return image
+        row,col=len(image),len(image[0])
+        oldColor=image[sr][sc]
+        q=deque()
+        q.append((sr,sc))
+        image[sr][sc]=color
+        directions=[[1,0],[-1,0],[0,1],[0,-1]]
+        while q:
+            r,c=q.popleft()
+            for m,n in directions:
+                newR,newC=r+m,c+n
+                if newR in range(row) and newC in range(col) and image[newR][newC]==oldColor:
+                    q.append((newR,newC))
+                    image[newR][newC]=color
+        return image
+image = [[1,1,1],[1,1,0],[1,0,1]]
+sr = 1
+sc = 1
+color = 2
+obj=Solution()
+print(obj.floodFill(image,sr,sc,color))  #Output: [[2,2,2],[2,2,0],[2,0,1]]
+
+        
+
+############################################
 #       DONE by DFS
 ############################################
 
