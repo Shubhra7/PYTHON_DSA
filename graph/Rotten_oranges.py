4ca87e0598c837aa4e1eb921443e8d5faa21fc20
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-06-11 23:04:47 +0530

    Rotten oranges

---
+++ graph/Rotten_oranges.py
@@ -0,0 +1,45 @@
+# https://leetcode.com/problems/rotting-oranges/
+
+"""
+Input: grid = [[2,1,1],[1,1,0],[0,1,1]]
+Output: 4
+"""
+import collections
+class Solution:
+    def orangesRotting(self, grid):
+        row=len(grid)
+        col=len(grid[0])
+
+        q=collections.deque()
+        fresh=set()
+        direction=[[1,0],[-1,0],[0,1],[0,-1]]
+
+        for i in range(row):
+            for j in range(col):
+                if (grid[i][j]==2):
+                    q.append((i,j,0))
+                if (grid[i][j]==1):
+                    fresh.add((i,j))
+
+        ans=0
+        while q:
+            r,c,time = q.popleft()
+            for i,j in direction:
+                rows = r + i
+                cols= c + j
+                if(rows in range(row) and cols in range(col)
+                    and grid[rows][cols]==1):
+                    grid[rows][cols]=2
+                    q.append((rows,cols,time+1))
+                    fresh.pop()
+                    ans=max(ans,time+1)
+
+        if (len(fresh)==0):
+            return ans
+        else:
+            return -1
+                
+obj=Solution()
+grid = [[2,1,1],[0,1,1],[1,0,1]] #-1
+# grid = [[2,1,1],[1,1,0],[0,1,1]]  #4
+print(obj.orangesRotting(grid))
