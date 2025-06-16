c7cf7c1434a2de7751ddf1710e3ace8f925bfbff
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-06-16 15:43:16 +0530

    Refactor string decoding logic to implement a function that expands encoded strings

--- test1.py
+++ test1.py
@@ -1,7 +1,26 @@
-arr=list(input())
-vol="aeiou"
-ans=""
-for i in arr:
-    if i not in vol:
-        ans+=i
-print(ans)
+from collections import deque
+def check(st):
+    nd,sd = deque(),deque()
+    for i in range(len(st)):
+        if st[i].isdigit():
+            nd.append(st[i])
+        elif st[i]!=']':
+            sd.append(st[i])
+        else:
+            ans=""
+            ele=""
+            allSt=""
+            while ele!='[':
+                allSt=ele+allSt
+                ele=sd.pop()   
+            cn=nd.pop()
+            ans=allSt*int(cn)
+            sd.append(ans)
+    return sd
+
+# st="3[a2[c]]"  #accaccacc
+st="3[a]2[bc]"  #aaabcbc
+print("".join(check(st)))
+
+
+
