89872b79023582a8fa5bde6155cf4be50d08d9bd
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-06-21 10:34:42 +0530

    Refactor cycle detection implementation to use a single array for tracking visit status

--- graph/detect_cycle_in_directed_graph.py
+++ graph/detect_cycle_in_directed_graph.py
@@ -38,4 +38,40 @@ V = 4
 edges= [[0, 1], [0, 2], [1, 2], [2, 3]]  #False
 obj=Solution()
 print("Cycle detection check: ",end=" ")
-print(obj.isCycle(V,edges))       
+print(obj.isCycle(V,edges))    
+
+################################
+#   Using Single array: visit (1), pathVisited (2)
+################################
+
+class Solution:
+    def isCycle(self, V, edges):
+        # code here
+        def dfs(node,adj,visit):
+            visit[node]=2
+            for adjN in adj[node]:
+                if visit[adjN]==0:
+                    if dfs(adjN,adj,visit):
+                        return True
+                elif visit[adjN]==2:
+                    return True
+            visit[node]=1
+            return False
+            
+        adj=[[] for _ in range(V)]
+        for u,v in edges:
+            adj[u].append(v)
+            
+        visit=[0]*V
+        for i in range(V):
+            if(visit[i]==0):
+                if(dfs(i,adj,visit)):
+                    return True
+        return False
+
+V = 4
+# edges =[[0, 1], [0, 2], [1, 2], [2, 0], [2, 3]]   #True
+edges= [[0, 1], [0, 2], [1, 2], [2, 3]]  #False
+obj=Solution()
+print("Cycle detection check: ",end=" ")
+print(obj.isCycle(V,edges))   
