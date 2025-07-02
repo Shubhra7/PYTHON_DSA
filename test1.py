93ef57e5dbd210955030a09b5fd5662687270409
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-07-02 13:11:01 +0530

    Refactor code to implement alien dictionary order checker

--- test1.py
+++ test1.py
@@ -1,26 +1,25 @@
-from collections import deque
-def check(st):
-    nd,sd = deque(),deque()
-    for i in range(len(st)):
-        if st[i].isdigit():
-            nd.append(st[i])
-        elif st[i]!=']':
-            sd.append(st[i])
-        else:
-            ans=""
-            ele=""
-            allSt=""
-            while ele!='[':
-                allSt=ele+allSt
-                ele=sd.pop()   
-            cn=nd.pop()
-            ans=allSt*int(cn)
-            sd.append(ans)
-    return sd
-
-# st="3[a2[c]]"  #accaccacc
-st="3[a]2[bc]"  #aaabcbc
-print("".join(check(st)))
-
-
-
+class Solution:
+    def isAlienSorted(self, words, order):
+        d = {ch: i for i, ch in enumerate(order)}
+        n = len(words)
+        for i in range(n - 1):
+            st1 = words[i]
+            st2 = words[i + 1]
+            leng = min(len(st1), len(st2))
+            for j in range(leng):
+                if st1[j] != st2[j]:
+                    if d[st1[j]] > d[st2[j]]:
+                        return False
+                    break  # Found differing char, stop comparing further
+            else:
+                # If all characters so far are same, longer word should not come before shorter one
+                if len(st1) > len(st2):
+                    return False
+        return True
+        
+        
+obj=Solution()
+words = ["word","world","row"]
+# words = ["hello","leetcode"]
+order = "hlabcdefgijkmnopqrstuvwxyz"
+print(obj.isAlienSorted(words,order))
