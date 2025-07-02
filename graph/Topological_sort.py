cbaa28aebe715862471b742d3c1ec385f9ccb568
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-07-02 14:33:49 +0530

    toposort using dfs done

--- graph/Topological_sort.py
+++ graph/Topological_sort.py
@@ -8,6 +8,43 @@ Explanation: The output true denotes that the order is valid. Few valid Topologi
 [1, 2, 3, 0]
 [2, 3, 1, 0]
 """
+######################################
+# For dfs just this part:
+#  https://youtu.be/ZUFQfFaU-8U?list=PLgUwDviBIf0oE3gA41TKO2H5bHpPd7fzn&t=309
+######################################
+
+
+
+class Solution:
+    def topoSort(self, V, edges):
+        # Code here
+        ###########################
+        #  USing DFS
+        ############################
+        def dfs(node,adj,ds,visit):
+            visit[node]=1
+            for it in adj[node]:
+                if visit[it]==0:
+                    dfs(it,adj,ds,visit)
+            ds.append(node)
+
+        adj=[[] for _ in range(V)]
+        visit=[0]*V
+        for u,v in edges:
+            adj[u].append(v)
+        ds=[]
+        for i in range(V):
+            if visit[i]==0:
+                dfs(i,adj,ds,visit)
+        ans=list(reversed(ds))
+        return ans
+         
+V = 4
+E = 3
+edges= [[3, 0], [1, 0], [2, 0]]
+obj=Solution()
+print("Using dfs do topo: ", end=" ")
+print(obj.topoSort(V,edges))
 
 # First write the indegree
 # whose indegree are 0 are pushed into the queue
