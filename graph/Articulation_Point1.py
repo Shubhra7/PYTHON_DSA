bec94c7f7f2c111fe121e8afc70cc76163e4ced9
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-08-02 19:11:16 +0530

    Articulation_point_1 .py

---
+++ graph/Articulation_Point1.py
@@ -0,0 +1,50 @@
+# https://www.geeksforgeeks.org/problems/articulation-point-1/1
+# https://youtu.be/j1QDfU21iZk
+
+
+import sys
+sys.setrecursionlimit(10**6)
+
+class Solution:
+    
+    #Function to return Breadth First Traversal of given graph.
+    def articulationPoints(self, V, adj):
+        def dfs(node,parent,tin,low,visit,adj,timer,mark):
+            visit[node]=True
+            tin[node]=timer
+            low[node]=timer
+            timer += 1
+            child=0
+            for it in adj[node]:
+                if it==parent:
+                    continue
+                if not visit[it]:
+                    dfs(it,node,tin,low,visit,adj,timer,mark)
+                    low[node] = min(low[node],low[it])
+                    if low[it]>=tin[node] and parent!=-1:
+                        mark[node]=1
+                    child += 1
+                else:
+                    low[node] = min(low[node],tin[it])
+            if child>1 and parent==-1:
+                mark[node]=1
+        # code here
+        tin=[-1]*V
+        low=[-1]*V
+        visit = [False]*V
+        mark=[0]*V
+        for i in range(V):
+            if not visit[i]:
+                dfs(i,-1,tin,low,visit,adj,0,mark)
+        ans=[]
+        for i in range(V):
+            if mark[i]==1:
+                ans.append(i)
+        if len(ans)==0:
+            return [-1]
+        return ans
+                
+adj=[[1], [0, 4], [4, 3], [4, 2], [1, 2, 3]]
+V=5
+obj=Solution()
+print(obj.articulationPoints(V,adj))
