0fb257179589e4937993544e7a91210e6cbf6826
Shubhra7 <shubhrajit.g2002@gmail.com>
2025-08-17 12:44:38 +0530

    done testing

--- DP/testdp.py
+++ DP/testdp.py
@@ -1,20 +1,16 @@
-s1,s2=map(str,input().split())
-n,m=len(s1),len(s2)
-dp=[[-1]*(m+1) for _ in range(n+1)]
-maxi=0
-ro,co=-1,-1
-for i in range(1,n+1):
-    for j in range(1,m+1):
-        if s1[i-1]==s2[j-1]:
-            dp[i][j]=dp[i-1][j-1]+1
-            if maxi<dp[i][j]:
-                maxi=dp[i][j]
-                ro,co=i,j
-        else:
-            dp[i][j]=0
-print(maxi)
-ans=""
-for i in range(ro,ro-maxi,-1):
-    ans=s1[i-1]+ans
+def permu(ind,arr,ds,ans):
+    if ind == len(arr):
+        ans.append(ds[:])
+    for i in range(ind,len(arr)):
+        ds.append(arr[i])
+        arr[i],arr[ind] = arr[ind],arr[i]
+        permu(ind+1,arr,ds,ans)
+        ds.pop()
+        arr[i],arr[ind] = arr[ind],arr[i]
+
+arr=[1,2,3,4]
+ds=[]
+ans=[]
+permu(0,arr,ds,ans)
 print(ans)
-    
+print(len(ans))
